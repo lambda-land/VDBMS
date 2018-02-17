512081994d0423c35e68edd6e594a0524005d453
Eric Walkingshaw <eric@walkingshaw.net>
2018-02-16 18:18:28 -0800

    Add attribute and relation names.

--- src/VDB/Feature.hs
+++
@@ -1,9 +0,0 @@
-module VDB.Feature where
-
-import Data.Data (Data,Typeable)
-import Data.String (IsString)
-
-
--- | A feature is a named, boolean configuration option.
-newtype Feature = Feature { featureName :: String }
-  deriving (Data,Eq,IsString,Ord,Show,Typeable)
---
+++ src/VDB/Name.hs
@@ -0,0 +1,17 @@
+module VDB.Name where
+
+import Data.Data (Data,Typeable)
+import Data.String (IsString)
+
+
+-- | A feature is a named, boolean configuration option.
+newtype Feature = Feature { featureName :: String }
+  deriving (Data,Eq,IsString,Ord,Show,Typeable)
+
+-- | An attribute (i.e. column) name.
+newtype Attribute = Attribute { attributeName :: String }
+  deriving (Data,Eq,IsString,Ord,Show,Typeable)
+
+-- | A relation (i.e. table) name.
+newtype Relation = Relation { relationName :: String }
+  deriving (Data,Eq,IsString,Ord,Show,Typeable)
