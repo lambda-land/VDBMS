df39793ca3fcf161e9424a41beb07eb4d9246aa5
Qiaoran Li <qiaoran@10-249-171-137.wireless.oregonstate.edu>
2018-02-20 13:47:27 -0800

    apply choice in list, change Query expression, add creating table in grammar

--- src/VDB/SQL.hs
+++ src/VDB/SQL.hs
@@ -9,24 +9,64 @@ import VDB.Condition
 -- * Syntax for SQL
 --
 
--- | An Attr is an Attribute together with its feature choices.
-data Attr 
-   = Attribute  
-   | AttrCHOICE FeatureExpr Attribute Attribute
-  deriving (Eq,Show)
 
--- | An attrList is a list of Attr. Empty list is not allowed.
+-- | An attrList is a list of Attribute. Empty list is not allowed.
 data AttrList 
-   = Attr 
-   | AttrConcat Attr AttrList
+   = Attr Attribute  
+   | AttrCHOICE FeatureExpr AttrList AttrList
+   | AttrConcat AttrList AttrList
   deriving (Eq,Show)
 
--- | A Rname is the name of relation.
-type Rname = String
+
+-- | A Table is a list of table. Empty list is not allowed. 
+data RelationList 
+   = R Relation
+   | RelaitonCHOICE FeatureExpr RelationList RelationList 
+   | CROSSJOIN RelationList RelaitonList
+  deriving (Eq,Show)
+
+-- | FROM ... 
+data FromSentence  = FROM RelationList
+  deriving (Eq,Show)
+  
+-- | Where ...
+data WhereSentence = WHERE Condition
+
+-- | Query expression. SELECT ... FROM ... WHERE ...
+data Query = SFW AttrList FromSentence WhereSentence  
+  deriving (Eq,Show)
+
 
 -- | A Vrelation is the name of variational relation and its corresponding attribute list.
-data Vrelation = VR Rname AttrList
+data Vrelation = VR Relation AttrList
    deriving (Eq,Show)
+
+-- 
+-- * Traditional schema in SQL
+--   (Define a schema in traditional SQL by create one table per time)
+-- 
+
+
+-- | Type expression for data
+data DataType = I | B | S | NULL
+
+-- | Associate attribute with datatype
+data AttrAndType = AT Attibute DataType
+
+-- | List of combined Attribute and dataType
+data AttrAndTypeList 
+   = SingleAT AttrAndType
+   | ATList   AttrAndType AttrAndTypeList
+
+-- | CREATE TABLE expression
+data CreateRelation = CreateTable Relation AttrAndTypeList
+
+
+--
+-- * Variational schema
+--  (Relation associated with varialtional relation) 
+-- 
+
 
 -- | A VrelationList is a list of Vrelaiton which will contribute to a v-schema.
 --   Empty list is not allowed. 
@@ -38,25 +78,3 @@ data VRelationList
 -- | A v-schema involved with featureExpr. 
 data VSchema = ScheCHOICE FeatureExpr VRelationList
   deriving (Eq,Show)
-
--- | A TableName is the name of a table. 
---   Table is different with the relation. 
-type TableName = String
-
--- | A Table associated with featureExpr. 
-data Table
-   = TableName 
-   | TableCHOICE FeatureExpr Table Table 
-  deriving (Eq,Show)
-
--- | A Table is a list of table. Empty list is not allowed. 
-data TableList 
-   = Table 
-   | CROSSJOIN Table TableList
-  deriving (Eq,Show)
-
--- | Query expression. SELECT ... FROM ... WHERE ...
-data Query = SFW AttrList TableList Condition  
-  deriving (Eq,Show)
-
-
