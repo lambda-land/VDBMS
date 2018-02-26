ad0a2886366069ce78de78798a907ba8d096f406
Qiaoran Li <liqiao@oregonstate.edu>
2018-02-26 11:17:46 -0800

    add lexer

--- src/VDB/ParsingSQL.hs
+++ src/VDB/ParsingSQL.hs
@@ -1,5 +1,12 @@
 module VDB.ParsingSQL where 
 
+import Control.Monad (void)
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Expr
+import qualified Text.Megaparsec.Char.Lexer as L
+
 --  
 -- Concrete syntax for VDB.SQL
 -- 
@@ -68,3 +75,60 @@ module VDB.ParsingSQL where
 -- | vRelationList ::= vRelation
 --                   | vRelaiton, vRelaitonList
 -- | vSchema ::= featureExpr ? {vRelationList}
+
+
+--
+--  Lexer
+--
+
+-- | spaceConsumer: consume the whitespace, newline,
+--                  line comment out, block comment out 
+spaceConsumer :: Parser ()
+spaceConsumer = L.space space1 newline lineCmnt blockCmnt 
+  where lineCmnt = L.skipLineComment "--"
+        blockCmnt = L.skipBlockComment "/*" "*/"
+
+-- | Wrap parser for 'lexeme'
+lexeme :: Parser a -> Parser a 
+lexeme = L.lexeme spaceConsumer
+
+-- | A helper to parse symbols (special string)
+symbol :: String -> Parser String 
+symbol = L.symbol spaceConsumer
+
+-- | 'parens' parse things between parenthesis 
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+-- | 'integer' parses an integer
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+-- | 'comma' parses a comma ","
+comma :: Parser String 
+comma = symbol ","
+
+-- | parses the reservedwords and identifiers 
+reservedWord :: String -> Parser ()
+reservedword w = lexeme (string w *> notFollowedBy alphaNumChar)
+
+-- | list of reserved words
+reservedwords :: [String]
+reservedwords = ["SELECT", "FROM", "WHERE", "OR", "AND", "NOT"]
+
+identifier :: Parser String
+identifier = (lexeme . try) (p >>= check)
+  where
+    p       = (:) <$> letterChar <*> many alphaNumChar
+    check x = if x `elem` reservedwords
+                then fail $ "keyword " ++ show x ++ " cannot be an identifier"
+                else return x
+
+
+--
+-- Parser
+--
+
+
+
+
