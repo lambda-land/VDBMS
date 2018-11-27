45215c21d3f41aae9e1d15549afcb3ba94b9663e
Parisa Ataei <>
2018-11-26 17:03:55 -0800

    add brute force ...

---
+++ src/VDB/BruteForce/BruteForce.hs
@@ -0,0 +1,24 @@
+-- Sends queries from the brute force translation to the db 
+-- and gets the plain relational result
+module VDB.BruteForce.BruteForce where 
+
+--import Prelude hiding (EQ ,LT ,GT)
+-- import VDB.SQL 
+import VDB.Algebra
+import VDB.Name
+import qualified VDB.FeatureExpr as F
+import qualified VDB.Condition as C
+import qualified VDB.Target as T
+import VDB.Variational
+import VDB.Value  
+import VDB.BruteForce.BruteForceAlg2Sql
+import VDB.BruteForce.BruteForceAppSat
+import VDB.BruteForce.BruteForceSendQs
+--import VDB.DBsetup.EnronEmailDB
+
+import Data.Map
+
+--import Database.HDBC.Sqlite3
+import Database.HDBC
+
+
