9b344f4376b230e58d466ce7626c54dac1f186aa
Parisa Ataei <>
2018-12-02 15:49:23 -0800

    brute alg2sql

--- src/VDB/BruteForce/BruteForceAlg2Sql.hs
+++ src/VDB/BruteForce/BruteForceAlg2Sql.hs
@@ -13,6 +13,7 @@ import qualified VDB.Target as T
 import VDB.Variational
 import VDB.Value
 import VDB.Config  
+import VDB.VqueryConfigSem
 
 import Data.Text as T (Text, pack, append, concat)
 import Data.List (intercalate)
@@ -20,9 +21,18 @@ import Data.Maybe (catMaybes)
 
 type Query = T.Text
 
--- |
+-- | takes a variational query and a list of configurations
+--   and returns a list of relational sql queries tagged with
+--   their configuration.
 bruteAlg2Sql :: Algebra -> [Config Bool] -> [Variant Bool Query]
-bruteAlg2Sql = undefined
+bruteAlg2Sql q cs = map (bruteQ q) cs 
+  where
+    bruteQ :: Algebra -> Config Bool -> Variant Bool Query
+    bruteQ configuredQ c = case relTrans configuredQ of 
+      Just relQ -> (c, relQ)
+      _ -> error "configuring vquery went wrong!!"
+      where configuredQ = configureVquery q c
+
 
 
 -- | takes a vq and returns a "just text" if vq is a pure
@@ -55,42 +65,16 @@ relPrj as
   | otherwise                       = Just (intercalate ", " (catMaybes (attListName as)))
 
 -- | helper function for relPrj.
-attName :: Opt Attribute -> Maybe String
-attName (F.Lit True, a) = Just (attributeName a)
-attName _             = Nothing
+-- attName :: Opt Attribute -> Maybe String
+-- attName (F.Lit True, a) = Just (attributeName a)
+-- attName _             = Nothing
 
 -- | helper function for relPrj.
 attListName :: [Opt Attribute] -> [Maybe String]
 attListName = map attName 
-
-{-trans :: Algebra -> F.FeatureExpr -> [Opt Text]
-trans (SetOp s l r) ctxt = [setAux s lq rq | lq <- lres, rq <- rres]
   where 
-    lres = trans l ctxt
-    rres = trans r ctxt
-trans (Proj oas q)  ctxt = map (\(f, q') -> (f, T.concat ["select ", as, " from ", q'])) res
-  where 
-    res = trans q ctxt
-    as  = prjAux oas 
-trans (Sel c q)     ctxt = map (\(f, q') -> (f, T.concat ["select * from ", q', " where " , T.pack (show c)])) res
-  where res = trans q ctxt
-trans (AChc f l r)  ctxt = lres ++ rres
-  where 
-    lres = trans l (F.And f ctxt)
-    rres = trans r (F.And (F.Not f) ctxt)
-trans (TRef r)      ctxt = [(ctxt, T.append "select * from " (T.pack (relationName r)))]
-trans (Empty)       ctxt = [(ctxt, "select null")]
+    attName :: Opt Attribute -> Maybe String
+    attName (F.Lit True, a) = Just (attributeName a)
+    attName _             = Nothing
 
--- | helper function for Setop queries, i.e., union, diff, prod
-setAux :: SetOp -> Opt Text -> Opt Text -> Opt Text
-setAux Union = \(lo, l) (ro, r) -> ((F.Or lo ro), T.concat [l, " union ", r])
-setAux Diff  = \(lo, l) (ro, r) -> ((F.And lo (F.Not ro)), T.concat [l, " minus ", r])
-setAux Prod  = \(lo, l) (ro, r) -> ((F.Or lo ro), T.concat ["select * from (" , l, ") join (", r, ")"])
 
--- | helper function for the projection query
-prjAux :: [Opt Attribute] -> Text
-prjAux [(o,a)] = T.pack (attributeName a)
-prjAux ((o,a):oas) = T.append (T.concat [T.pack(attributeName a), ", "]) (prjAux oas)
-prjAux [] = " "
-
--}
