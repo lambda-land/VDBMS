ff4c2601bed2264a817a47ec5001f440ebd0f5f8
Parisa Ataei <>
2019-01-15 13:02:39 -0800

    add vtable

--- src/VDB/VTable.hs
+++ src/VDB/VTable.hs
@@ -1,6 +1,6 @@
 -- Sends queries from the brute force translation to the db 
 -- and gets the plain relational result
-module VDB.Vresult where 
+module VDB.VTable where 
 
 --import Prelude hiding (EQ ,LT ,GT)
 -- import VDB.SQL 
@@ -9,24 +9,31 @@ module VDB.Vresult where
 -- import qualified VDB.FeatureExpr as F
 -- import qualified VDB.Condition as C
 -- import qualified VDB.Target as T
--- import VDB.Variational
+import VDB.Variational
 -- import VDB.Type  
--- import VDB.Schema
+import VDB.Schema
 -- import VDB.BruteForce.BruteForceSendQs
-import VDB.SqlTable (SqlVariantTable)
+import VDB.SqlTable 
 
 -- import Data.Map
 
 -- import Database.HDBC
 
+-- | the result of a vq is a variational table.
+--   variational table data type.
+type TableSchema = Opt RowType
+data VTable = VTable TableSchema  SqlTable
+
+
+
 -- | final result that is being showed to the user
-type PrettyVResult = String
+-- type PrettyVResult = String
 
 -- | the one variational table that is the result of user's query.
-type VResult = String
+-- type VResult = String
 
-prettyVres :: VResult -> PrettyVResult
-prettyVres = undefined
+-- prettyVres :: VResult -> PrettyVResult
+-- prettyVres = undefined
 
-packVres :: [SqlVariantTable] -> VResult 
-packVres = undefined
+-- packVres :: [SqlVariantTable] -> VResult 
+-- packVres = undefined
