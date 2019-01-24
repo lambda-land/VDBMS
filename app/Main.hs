cf0d6b1deda6cbf7745786ee77b838e795a4f885
Parisa Ataei <>
2019-01-24 13:30:36 -0800

    app1 works on rel q

--- app/Main.hs
+++ app/Main.hs
@@ -27,9 +27,12 @@ main = do
       employeeVDB = VDB employeeVSchema employeeConn
       p = PresCondAtt "presCond"
       q = empQ2_v1
+{-
+VTable (FAlSE OR ((((((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND NOT v4) OR (NOT ((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND v4)) AND NOT v5) OR (NOT ((((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND NOT v4) OR (NOT ((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND v4)) AND v5)),fromList [(Attribute {attributeQualifier = Nothing, attributeName = "title"},((((((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND NOT v4) OR (NOT ((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND v4)) AND NOT v5) OR (NOT ((((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND NOT v4) OR (NOT ((((v1 AND NOT v2) OR (NOT v1 AND v2)) AND NOT v3) OR (NOT ((v1 AND NOT v2) OR (NOT v1 AND v2)) AND v3)) AND v4)) AND v5),TByteString))]) [fromList [("title",SqlByteString "Senior Engineer")],fromList [("title",SqlByteString "Staff")],fromList [("title",SqlByteString "Engineer")],fromList [("title",SqlByteString "Senior Staff")],fromList [("title",SqlByteString "Assistant Engineer")],fromList [("title",SqlByteString "Technique Leader")]]
+-}      
       -- qualifyQuery employeeVSchema $ variationizeQuery [empQ2_v1]
-      -- vq = variationizeQuery [empQ1_v1, empQ1_v2, empQ1_v3, empQ1_v4, empQ1_v5]
-      -- qualifiedVq = qualifyQuery employeeVSchema vq
+      vq = variationizeQuery [empQ1_v1, empQ1_v2, empQ1_v3, empQ1_v4, empQ1_v5]
+      qualifiedVq = qualifyQuery employeeVSchema vq
   -- runTransFilterUnion qualifiedVq p employeeVDB
   -- runTransFilterUnion q p employeeVDB
   quickQuery' employeeConn "select * from v_job" []
