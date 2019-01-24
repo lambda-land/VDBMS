9c7c9c47b4c3ba84c96a307dc3fc432af1d2b752
Parisa Ataei <>
2019-01-24 11:05:59 -0800

    main

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,12 @@ import VDB.DBsetup.EnronEmailDB
 
 import Database.HDBC.Sqlite3
 
-main :: IO Connection
-main = enronEmail
+
+main :: IO [[SqlValue]]
+main = do
+  employeeConn <- connectSqlite3 "/databases/employeeDB/emp_vdb.db"
+  quickQuery' employeeConn "select * from v_job"
+
+-- main :: IO Connection
+-- main = enronEmail
 --main = return ()
