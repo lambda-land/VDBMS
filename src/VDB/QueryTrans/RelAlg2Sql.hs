354ef67cd09bf3e663a2a030b250e8481b3c0dcf
Parisa Ataei <>
2019-04-16 18:28:05 -0700

    haskell db working

--- src/VDB/QueryTrans/RelAlg2Sql.hs
+++ src/VDB/QueryTrans/RelAlg2Sql.hs
@@ -16,7 +16,7 @@ import VDB.Variant
 import Data.List (intercalate)
 import Data.Maybe (catMaybes)
 
--- import Database.HaskellDB
+import qualified Database.HaskellDB as HSDB
 
 type Query = String
 type VariantQuery = Variant Query Bool
