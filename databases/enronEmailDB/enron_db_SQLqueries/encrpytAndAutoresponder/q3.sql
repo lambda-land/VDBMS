20c3506b59e0b9d887c704298e06704220a3db6a
QiaoranChelsea <liqiao@oregonstate.edu>
2019-04-21 17:24:30 -0700

    q3:normal message auto respond + v_message has some duplicate problem

---
+++ databases/enronEmailDB/enron_db_SQLqueries/encrpytAndAutoresponder/q3.sql
@@ -0,0 +1,5 @@
+SELECT msg.subject as originalSubject, auto.subject as autoSubject, auto.body as autoBody
+FROM (v_recipientinfo rec
+inner join v_employee emp on rec.rvalue = emp.email_id)
+inner join v_auto_msg auto on auto.eid = emp.eid, message msg
+Where  rec.mid = 9138 and msg.mid = 9138
