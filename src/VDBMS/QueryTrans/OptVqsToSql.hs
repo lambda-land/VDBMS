a0f5935b3aa1213e104466c26d6c43bb88689828
Parisa Ataei <>
2019-05-27 15:17:30 -0700

    helper funcs

--- src/VDBMS/QueryTrans/OptVqsToSql.hs
+++ src/VDBMS/QueryTrans/OptVqsToSql.hs
@@ -6,23 +6,24 @@ module VDBMS.QueryTrans.OptVqsToSql where
 import Prelude hiding (Ordering(..))
 
 import qualified VDBMS.QueryLang.Algebra as A
-import VDBMS.VDB.Name
-import qualified VDBMS.Features.FeatureExpr.FeatureExpr as F
-import qualified VDBMS.QueryLang.Condition as C
-import VDBMS.DBMS.Value.Value
+-- import VDBMS.VDB.Name
+-- import qualified VDBMS.Features.FeatureExpr.FeatureExpr as F
+-- import qualified VDBMS.QueryLang.Condition as C
+-- import VDBMS.DBMS.Value.Value
 import VDBMS.Variational.Opt
-import VDBMS.TypeSystem
+-- import VDBMS.TypeSystem
 import VDBMS.VDB.Schema.Schema
-import VDBMS.Features.SAT 
+-- import VDBMS.Features.SAT 
 import VDBMS.QueryTrans.OptVqToOptSql 
 
 -- import qualified Database.HaskellDB as HSDB
 import qualified Database.HaskellDB.PrimQuery as P
-import Database.HDBC (SqlValue(..))
 
 
-allAttributes :: [Opt P.PrimQuery] -> P.Scheme 
-allAttributes = undefined
+allAttributes :: [P.PrimQuery] -> P.Scheme 
+allAttributes ps = undefined
+  where
+    as = nub $ map P.attributes ps
 
 nullAttributes :: P.PrimQuery -> P.Scheme -> P.Scheme
 nullAttributes = undefined
@@ -39,8 +40,8 @@ unionAll ps = undefined
 transOptVqs2Sql :: [Opt A.Algebra] -> Schema -> P.PrimQuery
 transOptVqs2Sql oqs s = unionAll prims
   where
-    oprims = mapSnd (flip transAlgebra2Sql s) oqs -- [Opt P.PrimExpr]
-    allAtts = allAttributes oprims
+    oprims  = mapSnd (flip transAlgebra2Sql s) oqs -- [Opt P.PrimExpr]
+    allAtts = allAttributes $ map getObj oprims
     oprims' = mapSnd (flip addNullAttsToPrj allAtts) oprims
-    prims = map concatFexp oprims'
+    prims   = map concatFexp oprims'
 
