d721ecd070c6ca4129194d8708b4cfa1d34d34db
Parisa Ataei <>
2019-05-29 21:06:59 -0700

    add missing files

---
+++ src/VDBMS/QueryTrans/Variational/AlgebraToOptSqls.hs
@@ -0,0 +1,28 @@
+-- | translates a variational query to a list of opt sqls.
+module VDBMS.QueryTrans.Variational.AlgebraToOptSqls (
+
+        trans
+
+) where 
+
+import VDBMS.QueryLang.Variational.Algebra
+import VDBMS.QueryTrans.Variational.LinearizeQuery
+import VDBMS.QueryTrans.Relational.AlgebraToSql
+import VDBMS.VDB.Schema.Schema
+-- import VDBMS.QueryLang.Relational.Algebra
+-- import VDBMS.Features.Config
+import VDBMS.Variational.Opt
+-- import VDBMS.Variational.Variational
+-- import qualified VDBMS.Features.FeatureExpr.FeatureExpr as F
+-- import VDBMS.VDB.Name
+-- import qualified VDBMS.QueryLang.Variational.Condition as C
+-- import VDBMS.QueryLang.Relational.Condition
+
+import Database.HaskellDB.PrimQuery 
+
+-- | translates a variational query to a list of sql queries.
+trans :: Algebra -> Schema -> [Opt PrimQuery]
+trans q s = mapSnd (flip transAlgebra2Sql s) validQs
+  where
+    linearizedQs = linearize q
+    validQs = validifyOpts linearizedQs
---
+++ src/VDBMS/QueryTrans/Variational/Test583.hs
@@ -0,0 +1,18 @@
+-- | translates a variational query to a list of opt sqls.
+module VDBMS.QueryTrans.Variational.Test583 where 
+
+import VDBMS.QueryTrans.Variational.AlgebraToOptSqls
+-- import VDBMS.QueryTrans.Variational.LinearizeQuery
+-- import VDBMS.QueryTrans.Relational.AlgebraToSql
+-- import VDBMS.VDB.Schema.Schema
+-- import VDBMS.QueryLang.Relational.Algebra
+-- import VDBMS.Features.Config
+-- import VDBMS.Variational.Opt
+-- import VDBMS.Variational.Variational
+-- import qualified VDBMS.Features.FeatureExpr.FeatureExpr as F
+-- import VDBMS.VDB.Name
+-- import qualified VDBMS.QueryLang.Variational.Condition as C
+-- import VDBMS.QueryLang.Relational.Condition
+
+import Database.HaskellDB.PrimQuery 
+
