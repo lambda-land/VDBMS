c9eb279b8dbd371bce75a202a855293f92d00de2
Parisa Ataei <>
2019-05-29 20:05:09 -0700

    comments

--- src/VDBMS/QueryLang/VariationalToRelational/LinearizeQuery.hs
+++ src/VDBMS/QueryLang/VariationalToRelational/LinearizeQuery.hs
@@ -11,7 +11,7 @@ import VDBMS.VDB.Name
 import qualified VDBMS.QueryLang.Variational.Condition as C
 import VDBMS.QueryLang.Relational.Condition
 
-
+-- | Linearizes variational conditions to a list of opt relational conditions.
 linearizeCond :: C.Condition -> [Opt RCondition]
 linearizeCond (C.Lit b)        = pure $ mkOpt (F.Lit True) (RLit b)
 linearizeCond (C.Comp c a1 a2) = pure $ mkOpt (F.Lit True) (RComp c a1 a2)
@@ -21,7 +21,7 @@ linearizeCond (C.And c1 c2)    = combOpts F.And RAnd (linearizeCond c1) (lineari
 linearizeCond (C.CChc f c1 c2) = mapFst (F.And f) (linearizeCond c1) ++
                                  mapFst (F.And (F.Not f)) (linearizeCond c2)
 
-
+-- | Linearizes a variational query to a list of opt relational queries.
 linearize :: Algebra -> [Opt RAlgebra]
 linearize (SetOp s q1 q2) = combOpts F.And (RSetOp s) (linearize q1) (linearize q2)
 linearize (Proj as q)     = combOpts F.And RProj (groupOpts as) (linearize q)
