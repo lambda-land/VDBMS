47c33c7d2f37bb08d93666c28222b9cad3969897
pataei <paris.ataei@gmail.com>
2019-06-02 21:56:14 -0700

    print sql qs

--- src/VDBMS/QueryTrans/Variational/Test583.hs
+++ src/VDBMS/QueryTrans/Variational/Test583.hs
@@ -7,7 +7,7 @@ import VDBMS.QueryTrans.Variational.AlgebraToOptSqls
 import VDBMS.VDB.Schema.Schema
 import VDBMS.QueryLang.Variational.Algebra
 -- import VDBMS.Features.Config
--- import VDBMS.Variational.Opt
+import VDBMS.Variational.Opt
 -- import VDBMS.Variational.Variational
 -- import qualified VDBMS.Features.FeatureExpr.FeatureExpr as F
 -- import VDBMS.VDB.Name
@@ -16,11 +16,12 @@ import VDBMS.QueryLang.Variational.Algebra
 
 import Database.HaskellDB.PrimQuery 
 import Database.HaskellDB.Sql.Print
-import Database.HaskellDB.Sql.Generate
+-- import Database.HaskellDB.Sql.Generate 
+import Database.HaskellDB.Sql.Default (defaultSqlQuery, defaultSqlGenerator)
 
 import Text.PrettyPrint (Doc)
 
-printSql :: Algebra -> Schema -> Doc
-printSql q s = mapSnd (ppSql (sqlQuery defaultSqlGenerator)) primQs
-  where
-    primQs = trans q s 
+printSql :: Algebra -> Schema -> [Opt Doc]
+printSql q s = mapSnd (ppSql . (defaultSqlQuery defaultSqlGenerator)) 
+                    $ trans q s 
+
