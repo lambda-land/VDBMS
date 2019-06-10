079d469c6b74e3ecb98f12f3efcb46fddae9f6ad
pataei <paris.ataei@gmail.com>
2019-06-10 12:17:57 -0700

    renamed modules

---
+++ src/VDBMS/VDB/Database/Sqlite3/Sqlite.hs
@@ -0,0 +1,28 @@
+-- | Sqlite database.
+module VDBMS.VDB.Database.Sqlite3.Sqlite where
+
+-- import VDBMS.Features.Config
+-- import VDBMS.Features.ConfFexp
+import VDBMS.VDB.Schema.Schema
+-- import VDBMS.VDB.Table.Table
+import VDBMS.VDB.Name
+import VDBMS.VDB.Database.Database
+
+import qualified Database.SQLite3 as S
+import Data.Text
+
+-- | A database residing in Sqlite and using sqlite3
+--   to connect to it. 
+data Sqlite3 = Sqlite3 PresCondAtt Schema S.Database
+
+instance Database Sqlite3 where
+  type Path Sqlite3 = Text
+  connect t p s = do conn <- S.open t
+                     return $ Sqlite3 p s conn
+  disconnect (Sqlite3 p s c) = S.close c
+  schema (Sqlite3 p s c) = s 
+  presCond (Sqlite3 p s c) = p
+  runQ = undefined
+
+
+-- ex1 = Sqlite3 "../../../databases/testDB/test1.db" 
