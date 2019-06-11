290d026690033a41d9a1f1116c2e0a95a7344f25
pataei <paris.ataei@gmail.com>
2019-06-11 11:04:13 -0700

    not using monad catch here

--- src/VDBMS/QueryLang/Basics/Atom.hs
+++ src/VDBMS/QueryLang/Basics/Atom.hs
@@ -6,12 +6,16 @@ module VDBMS.QueryLang.Basics.Atom (
 ) where
 
 import Data.Data (Data,Typeable)
+-- import GHC.Generics (Generic)
 -- import Data.SBV (Boolean(..))
 import Data.Convertible (safeConvert)
 -- import qualified Data.Text as T (pack,Text)
+-- import Control.Monad.Catch (MonadThrow)
+-- import Control.Monad.Either (runEitherT)
 
 import VDBMS.VDB.Name
 import VDBMS.DBMS.Value.Value
+
 
 import Database.HDBC (SqlValue)
 
@@ -21,9 +25,13 @@ data Atom
    | Attr Attribute
   deriving (Data,Eq,Typeable,Ord)
 
+-- data AtomError = UnsafeConversion Atom
+--   deriving (Data,Eq,Generic,Ord,Show,Typeable)
+
+
 -- | pretty print atoms.
 prettyAtom :: Atom -> String
-prettyAtom (Val v)  = case safeConvert v of 
+prettyAtom (Val v)  =  case safeConvert v of 
   Right val -> val
   _ -> error "safeConvert resulted in error!!! showAtom"
 prettyAtom (Attr a) = getAttName a
