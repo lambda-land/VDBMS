59155dfd27a84cddfedcb3af94589e70f38a693a
pataei <paris.ataei@gmail.com>
2019-06-24 12:58:03 -0700

    add law ... need help

--- src/VDBMS/Variational/Variational.hs
+++ src/VDBMS/Variational/Variational.hs
@@ -18,33 +18,38 @@ class Variational a where
   -- | The nonvariational correspondent of a.
   type NonVariational a :: *
 
-  -- type Configr a :: *
+  -- | A configured thing with its configuration
+  --   attached to it.
+  -- type Configured a :: *
+
   -- | The variant of a variational thing, i.e., 
   --   the non-variational thing tagged with a fexp.
   type Variant a :: *
 
-
   -- | Fully configure a variational thing and return the
   --   non-variational correspondent of it.
   configure :: Config Bool -> a -> NonVariational a
+
+  -- | configures a variaitonal thing for all valid configurations
+  --   possible and generates a list of configured things.
+  -- configr :: a ->  [Configured a]
 
   -- | Linearizes a variational thing by generating a list
   --   of non-variational things with fexp attached to them.
   linearize :: a -> [Variant a]
 
   -- | Map a function of non-variational things to
-  --   variaitonal things.
-  -- vmap :: (Variational b) 
+  --   variaitonal things. Basically lifts a non-variational
+  --   function to the variational level.
+  -- vmap :: (Variational a, Variational b) 
   --   => (NonVariational a -> NonVariational b)
   --   -> a -> b
 
--- Alex idea:
--- class V v where
---   type NV v :: *
---   type Config v :: *
---   config :: Config v -> v -> NV v
---   vmap :: (V u) => (NV v -> NV u) -> v -> u
---   law: f (config c v) = config c ((vmap f) v)
+  -- | configuring a variational thing first and applying
+  --   a function on it is equivalent to applying that 
+  --   function on the variational thing first and then
+  --   configuring it.
+  -- law: f (configure c v) = configure c ((vmap f) v)
 
 
 
