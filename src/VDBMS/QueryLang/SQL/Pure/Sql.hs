76b4de99a87975f910dffc1604430fa2702c87c1
pataei <paris.ataei@gmail.com>
2019-06-27 13:30:02 -0700

    clean up

--- src/VDBMS/QueryLang/SQL/Pure/Sql.hs
+++ src/VDBMS/QueryLang/SQL/Pure/Sql.hs
@@ -38,35 +38,3 @@ data SqlTempRes =
     SqlWith [Rename SqlSelect] SqlSelect
   | SqlView (Rename SqlSelect)
 
--- 
--- haskelldb sql type:
---
--- data SqlSelect  = SqlSelect { 
---                              options   :: [String],                -- ^ DISTINCT, ALL etc.
---            attrs     :: [(SqlColumn,SqlExpr)],   -- ^ result
---                              tables    :: [(SqlTable,SqlSelect)],  -- ^ FROM
---                              criteria  :: [SqlExpr],               -- ^ WHERE
---                              groupby   :: Maybe Mark,   -- ^ GROUP BY
---                              orderby   :: [(SqlExpr,SqlOrder)],    -- ^ ORDER BY
---            extra     :: [String]                 -- ^ TOP n, etc.
---                             }
---                 | SqlBin   String SqlSelect SqlSelect -- ^ Binary relational operator
---                 | SqlTable SqlTable -- ^ Select a whole table.
---                 | SqlEmpty -- ^ Empty select.
---   deriving Show
-
--- data SqlExpr = ColumnSqlExpr  SqlColumn
---              | BinSqlExpr     String SqlExpr SqlExpr
---              | PrefixSqlExpr  String SqlExpr
---              | PostfixSqlExpr String SqlExpr
---              | FunSqlExpr     String [SqlExpr]
---              | AggrFunSqlExpr String [SqlExpr] -- ^ Aggregate functions separate from normal functions.
---              | ConstSqlExpr   String
---        | CaseSqlExpr    [(SqlExpr,SqlExpr)] SqlExpr
---              | ListSqlExpr    [SqlExpr]
---              | ExistsSqlExpr  SqlSelect
---              | ParamSqlExpr (Maybe SqlName) SqlExpr
---              | PlaceHolderSqlExpr
---              | ParensSqlExpr SqlExpr
---              | CastSqlExpr String SqlExpr 
---   deriving Show
