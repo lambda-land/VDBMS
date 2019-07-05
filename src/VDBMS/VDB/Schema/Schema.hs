7e3f0fed67f3e50e88b345965dc28af49c126a81
pataei <paris.ataei@gmail.com>
2019-07-05 12:25:08 -0700

    config schema

--- src/VDBMS/VDB/Schema/Schema.hs
+++ src/VDBMS/VDB/Schema/Schema.hs
@@ -4,7 +4,7 @@ module VDBMS.VDB.Schema.Schema (
         module VDBMS.VDB.Schema.Types,
         module VDBMS.VDB.Schema.Lookups,
         module VDBMS.VDB.Schema.Core,
-        module VDBMS.VDB.Schema.ApplyConf,
+        -- module VDBMS.VDB.Schema.ApplyConf,
         module VDBMS.VDB.Schema.ApplyFexp,
         module VDBMS.VDB.Schema.Equiv
 
@@ -13,7 +13,7 @@ module VDBMS.VDB.Schema.Schema (
 import VDBMS.VDB.Schema.Types
 import VDBMS.VDB.Schema.Lookups 
 import VDBMS.VDB.Schema.Core
-import VDBMS.VDB.Schema.ApplyConf
+-- import VDBMS.VDB.Schema.ApplyConf
 import VDBMS.VDB.Schema.ApplyFexp
 import VDBMS.VDB.Schema.Equiv
 
