067af00b0b8ce22216e1b49fff58b8bc727c7300
pataei <paris.ataei@gmail.com>
2019-07-16 18:58:20 -0700

    start of type systems wrt aliasing

--- src/VDBMS/TypeSystem/Relational/TypeSystem.hs
+++ src/VDBMS/TypeSystem/Relational/TypeSystem.hs
@@ -24,18 +24,19 @@ import VDBMS.QueryLang.RelAlg.Relational.Algebra
 import VDBMS.VDB.Name
 import VDBMS.DBMS.Value.Value (typeOf,SqlType)
 
-
-
 -- | Attribute information for relational type env.
 data RAttrInfo 
   = RAttrInfo {
       rAttrType :: SqlType
     , rAttrQuals :: [Qualifier]
     }
- deriving (Eq,Show,Typeable)
+ deriving (Data,Ord,Eq,Show,Typeable)
 
 -- | Relatioanl type enviornment.
-type RTypeEnv = RTableSchema
+-- type RTypeEnv = RTableSchema
+
+-- | Relational type env.
+type RTypeEnv = M.Map Attribute RAttrInfo
 
 -- | Type enviornment errors.
 data RTypeError = 
@@ -51,119 +52,120 @@ instance Exception RTypeError
 
 -- | static semantics that returns a relational table schema.
 typeOfRQuery :: MonadThrow m => RAlgebra -> RSchema -> m RTypeEnv
-typeOfRQuery (RSetOp o l r)    s =
-  do typel <- typeOfRQuery l s
-     typer <- typeOfRQuery r s
-     if typel == typer
-     then return typel
-     else throwM $ RNotEquiveTypeEnv typel typer
-typeOfRQuery (RProj as rq)     s =
-  do tq <- typeOfRQuery (thing rq) s
-     if null as 
-     then throwM $ REmptyAttrList (thing rq)
-     else if attsSubTypeEnv as tq
-          then return $ SM.restrictKeys tq $ attsSet as
-          else throwM $ RAttributesNotInTypeEnv as tq
-typeOfRQuery (RSel c rq)       s = 
-  do tq <- typeOfRQuery (thing rq) s
-     typeOfSqlCond c tq s 
-typeOfRQuery (RJoin js)        s = typeOfJoins js s
-typeOfRQuery (RProd rl rr rrs) s = 
-  do r <- lookupRelation (thing rl) s
-     l <- lookupRelation (thing rr) s
-     rs <- mapM (flip lookupRelation s . thing) rrs
-     if disjointTypeEnvs r l rs 
-     then return $ SM.unions $ r : l : rs
-     else throwM $ RNotDisjointRels $ fmap thing (rl : rr : rrs)
-typeOfRQuery (RTRef rr)        s = lookupRelation (thing rr) s
-typeOfRQuery REmpty            _ = return M.empty
+typeOfRQuery = undefined
+-- typeOfRQuery (RSetOp o l r)    s =
+--   do typel <- typeOfRQuery l s
+--      typer <- typeOfRQuery r s
+--      if typel == typer
+--      then return typel
+--      else throwM $ RNotEquiveTypeEnv typel typer
+-- typeOfRQuery (RProj as rq)     s =
+--   do tq <- typeOfRQuery (thing rq) s
+--      if null as 
+--      then throwM $ REmptyAttrList (thing rq)
+--      else if attsSubTypeEnv as tq
+--           then return $ SM.restrictKeys tq $ attsSet as
+--           else throwM $ RAttributesNotInTypeEnv as tq
+-- typeOfRQuery (RSel c rq)       s = 
+--   do tq <- typeOfRQuery (thing rq) s
+--      typeOfSqlCond c tq s 
+-- typeOfRQuery (RJoin js)        s = typeOfJoins js s
+-- typeOfRQuery (RProd rl rr rrs) s = 
+--   do r <- lookupRelation (thing rl) s
+--      l <- lookupRelation (thing rr) s
+--      rs <- mapM (flip lookupRelation s . thing) rrs
+--      if disjointTypeEnvs r l rs 
+--      then return $ SM.unions $ r : l : rs
+--      else throwM $ RNotDisjointRels $ fmap thing (rl : rr : rrs)
+-- typeOfRQuery (RTRef rr)        s = lookupRelation (thing rr) s
+-- typeOfRQuery REmpty            _ = return M.empty
 
--- | Static semantics of relational conditions.
-typeOfSqlCond :: MonadThrow m => SqlCond RAlgebra -> RTypeEnv -> RSchema -> m RTypeEnv
-typeOfSqlCond (SqlCond c)  t s = typeOfRCondition c t
-typeOfSqlCond (SqlIn a q)  t s = 
-  do t' <- typeOfRQuery q s
-     attInTypeEnv (attribute a) t' 
-typeOfSqlCond (SqlNot c)   t s = typeOfSqlCond c t s 
-typeOfSqlCond (SqlOr l r)  t s = 
-  do typeOfSqlCond l t s
-     typeOfSqlCond r t s
-typeOfSqlCond (SqlAnd l r) t s = 
-  do typeOfSqlCond l t s
-     typeOfSqlCond r t s
+-- -- | Static semantics of relational conditions.
+-- typeOfSqlCond :: MonadThrow m => SqlCond RAlgebra -> RTypeEnv -> RSchema -> m RTypeEnv
+-- typeOfSqlCond (SqlCond c)  t s = typeOfRCondition c t
+-- typeOfSqlCond (SqlIn a q)  t s = 
+--   do t' <- typeOfRQuery q s
+--      attInTypeEnv (attribute a) t' 
+-- typeOfSqlCond (SqlNot c)   t s = typeOfSqlCond c t s 
+-- typeOfSqlCond (SqlOr l r)  t s = 
+--   do typeOfSqlCond l t s
+--      typeOfSqlCond r t s
+-- typeOfSqlCond (SqlAnd l r) t s = 
+--   do typeOfSqlCond l t s
+--      typeOfSqlCond r t s
 
--- | Checks if the condition is compatible with type env.
-typeOfRCondition :: MonadThrow m => RCondition -> RTypeEnv -> m RTypeEnv
-typeOfRCondition (RLit b)      t = return t 
-typeOfRCondition (RComp _ l r) t = typeOfComp l r t 
-typeOfRCondition (RNot c)      t = typeOfRCondition c t
-typeOfRCondition (ROr l r)     t = 
-  do typeOfRCondition l t
-     typeOfRCondition r t
-typeOfRCondition (RAnd l r)    t = 
-  do typeOfRCondition l t
-     typeOfRCondition r t
+-- -- | Checks if the condition is compatible with type env.
+-- typeOfRCondition :: MonadThrow m => RCondition -> RTypeEnv -> m RTypeEnv
+-- typeOfRCondition (RLit b)      t = return t 
+-- typeOfRCondition (RComp _ l r) t = typeOfComp l r t 
+-- typeOfRCondition (RNot c)      t = typeOfRCondition c t
+-- typeOfRCondition (ROr l r)     t = 
+--   do typeOfRCondition l t
+--      typeOfRCondition r t
+-- typeOfRCondition (RAnd l r)    t = 
+--   do typeOfRCondition l t
+--      typeOfRCondition r t
 
--- | Checks if the type env is consistent with a comparison condition.
-typeOfComp :: MonadThrow m => Atom -> Atom -> RTypeEnv -> m RTypeEnv
-typeOfComp a@(Val l)  a'@(Val r)  t 
-  | typeOf l == typeOf r = return t 
-  | otherwise = throwM $ RCompInvalid a a' t 
-typeOfComp a@(Val l)  a'@(Att r) t = 
-  do attInTypeEnv (attribute r) t 
-     at <- lookupAttrType (attribute r) t
-     if typeOf l == at 
-     then return t 
-     else throwM $ RCompInvalid a a' t
-typeOfComp a@(Att l) a'@(Val r)  t = 
-  do attInTypeEnv (attribute l) t 
-     at <- lookupAttrType (attribute l) t
-     if typeOf r == at 
-     then return t 
-     else throwM $ RCompInvalid a a' t
-typeOfComp a@(Att l) a'@(Att r) t = 
-  do attInTypeEnv (attribute l) t 
-     attInTypeEnv (attribute r) t 
-     at  <- lookupAttrType (attribute l) t
-     at' <-  lookupAttrType (attribute r) t
-     if at == at'
-     then return t 
-     else throwM $ RCompInvalid a a' t
+-- -- | Checks if the type env is consistent with a comparison condition.
+-- typeOfComp :: MonadThrow m => Atom -> Atom -> RTypeEnv -> m RTypeEnv
+-- typeOfComp a@(Val l)  a'@(Val r)  t 
+--   | typeOf l == typeOf r = return t 
+--   | otherwise = throwM $ RCompInvalid a a' t 
+-- typeOfComp a@(Val l)  a'@(Att r) t = 
+--   do attInTypeEnv (attribute r) t 
+--      at <- lookupAttrType (attribute r) t
+--      if typeOf l == at 
+--      then return t 
+--      else throwM $ RCompInvalid a a' t
+-- typeOfComp a@(Att l) a'@(Val r)  t = 
+--   do attInTypeEnv (attribute l) t 
+--      at <- lookupAttrType (attribute l) t
+--      if typeOf r == at 
+--      then return t 
+--      else throwM $ RCompInvalid a a' t
+-- typeOfComp a@(Att l) a'@(Att r) t = 
+--   do attInTypeEnv (attribute l) t 
+--      attInTypeEnv (attribute r) t 
+--      at  <- lookupAttrType (attribute l) t
+--      at' <-  lookupAttrType (attribute r) t
+--      if at == at'
+--      then return t 
+--      else throwM $ RCompInvalid a a' t
 
--- | Checks if the type env includes an attribute.
-attInTypeEnv :: MonadThrow m => Attribute -> RTypeEnv -> m RTypeEnv
-attInTypeEnv a t 
-  | a `Set.member` SM.keysSet t = return t 
-  | otherwise                   = throwM $ RAttributeNotInTypeEnv a t
+-- -- | Checks if the type env includes an attribute.
+-- attInTypeEnv :: MonadThrow m => Attribute -> RTypeEnv -> m RTypeEnv
+-- attInTypeEnv a t 
+--   | a `Set.member` SM.keysSet t = return t 
+--   | otherwise                   = throwM $ RAttributeNotInTypeEnv a t
 
--- | Type of joins. Note that for now joins are only among relations
---   and not queries.
-typeOfJoins :: MonadThrow m => RJoins -> RSchema -> m RTypeEnv
-typeOfJoins (RJoinTwoTable rl rr c) s = 
-  do lt <- typeOfRQuery (RTRef rl) s
-     rt <- typeOfRQuery (RTRef rr) s
-     typeOfRCondition c (SM.union lt rt)
-typeOfJoins (RJoinMore js rr c)     s = 
-  do jt <- typeOfJoins js s 
-     rt <- typeOfRQuery (RTRef rr) s
-     typeOfRCondition c (SM.union jt rt)
+-- -- | Type of joins. Note that for now joins are only among relations
+-- --   and not queries.
+-- typeOfJoins :: MonadThrow m => RJoins -> RSchema -> m RTypeEnv
+-- typeOfJoins (RJoinTwoTable rl rr c) s = 
+--   do lt <- typeOfRQuery (RTRef rl) s
+--      rt <- typeOfRQuery (RTRef rr) s
+--      typeOfRCondition c (SM.union lt rt)
+-- typeOfJoins (RJoinMore js rr c)     s = 
+--   do jt <- typeOfJoins js s 
+--      rt <- typeOfRQuery (RTRef rr) s
+--      typeOfRCondition c (SM.union jt rt)
 
--- | Attributes are all included in the type env.
-attsSubTypeEnv :: Attributes -> RTypeEnv -> Bool
-attsSubTypeEnv as t = attsSet as `Set.isSubsetOf` SM.keysSet t
+-- -- | Attributes are all included in the type env.
+-- attsSubTypeEnv :: Attributes -> RTypeEnv -> Bool
+-- attsSubTypeEnv as t = attsSet as `Set.isSubsetOf` SM.keysSet t
 
--- | Checks if a non-empty list of type envs are disjoint or not.
---   Note that since we're adding type envs to the list we know 
---   for a fact that it isn't empty.
-disjointTypeEnvs :: RTypeEnv -> RTypeEnv -> [RTypeEnv] -> Bool
-disjointTypeEnvs l r ts 
-  | SM.keysSet l `Set.disjoint` SM.keysSet r = disjointAll $ l : r : ts
-  | otherwise = False
-    where
-      disjointAll (x : xs) = all (Set.disjoint (SM.keysSet x)) (fmap SM.keysSet xs) 
-                             && disjointAll xs
-      disjointAll [x]      = True
-      disjointAll []       = True
+-- -- | Checks if a non-empty list of type envs are disjoint or not.
+-- --   Note that since we're adding type envs to the list we know 
+-- --   for a fact that it isn't empty.
+-- disjointTypeEnvs :: RTypeEnv -> RTypeEnv -> [RTypeEnv] -> Bool
+-- disjointTypeEnvs l r ts 
+--   | SM.keysSet l `Set.disjoint` SM.keysSet r = disjointAll $ l : r : ts
+--   | otherwise = False
+--     where
+--       disjointAll (x : xs) = all (Set.disjoint (SM.keysSet x)) (fmap SM.keysSet xs) 
+--                              && disjointAll xs
+--       disjointAll [x]      = True
+--       disjointAll []       = True
 
 
 
--- src/VDBMS/TypeSystem/Variational/TypeSystem.hs
+++ src/VDBMS/TypeSystem/Variational/TypeSystem.hs
@@ -40,10 +40,13 @@ data AttrInfo
     , attrType :: SqlType
     , attrQuals :: [Qualifier]
     }
- deriving (Eq,Show,Typeable)
+ deriving (Data,Ord,Eq,Show,Typeable)
 
 -- | Variational type env.
-type TypeEnv = TableSchema
+-- type TypeEnv = TableSchema
+
+-- | Variational type env.
+type TypeEnv = M.Map Attribute AttrInfo
 
 -- | Possible typing errors.
 data TypeError 
@@ -63,9 +66,9 @@ instance Exception TypeError
 --   checks the satisfiability of all attributes' pres conds conjoined
 --   with table pres cond.
 verifyTypeEnv :: MonadThrow m => TypeEnv -> m TypeEnv
-verifyTypeEnv t
-  | satisfiable (getFexp t) = return $ propagateFexpToTsch t
-  | otherwise = throwM $ EnvFexpUnsat (getFexp t) t
+verifyTypeEnv t = undefined
+  -- | satisfiable (getFexp t) = return $ propagateFexpToTsch t
+  -- | otherwise = throwM $ EnvFexpUnsat (getFexp t) t
 
 -- 
 -- Static semantics of a vquery that returns a table schema,
@@ -74,212 +77,213 @@ verifyTypeEnv t
 typeOfQuery :: MonadThrow m 
              => Algebra -> VariationalContext -> Schema 
              -> m TypeEnv
-typeOfQuery (SetOp o l r)    ctx s = typeSetOp l r ctx s
-typeOfQuery (Proj oas rq)    ctx s = typeProj oas rq ctx s
-typeOfQuery (Sel c rq)       ctx s = 
-  do t <- typeOfQuery (thing rq) ctx s
-     typeVsqlCond c ctx s t 
-     appFexpTableSch ctx t
-typeOfQuery (AChc f l r)     ctx s = 
-  do tl <- typeOfQuery l (F.And ctx f) s
-     tr <- typeOfQuery r (F.And ctx (F.Not f)) s
-     return $ mkOpt (F.Or (getFexp tl) (getFexp tr)) 
-                  $ rowTypeUnion (getObj tl) (getObj tr)
-typeOfQuery (Join js)        ctx s = typeJoin js ctx s
-typeOfQuery (Prod rl rr rrs) ctx s = 
-  typeProd (thing rl) (thing rr) (fmap thing rrs) ctx s
-typeOfQuery (TRef rr)        ctx s = typeRel (thing rr) ctx s
-typeOfQuery Empty            ctx s = 
-  appFexpTableSch ctx $ mkOpt (F.Lit True) M.empty
+typeOfQuery = undefined
+-- typeOfQuery (SetOp o l r)    ctx s = typeSetOp l r ctx s
+-- typeOfQuery (Proj oas rq)    ctx s = typeProj oas rq ctx s
+-- typeOfQuery (Sel c rq)       ctx s = 
+--   do t <- typeOfQuery (thing rq) ctx s
+--      typeVsqlCond c ctx s t 
+--      appFexpTableSch ctx t
+-- typeOfQuery (AChc f l r)     ctx s = 
+--   do tl <- typeOfQuery l (F.And ctx f) s
+--      tr <- typeOfQuery r (F.And ctx (F.Not f)) s
+--      return $ mkOpt (F.Or (getFexp tl) (getFexp tr)) 
+--                   $ rowTypeUnion (getObj tl) (getObj tr)
+-- typeOfQuery (Join js)        ctx s = typeJoin js ctx s
+-- typeOfQuery (Prod rl rr rrs) ctx s = 
+--   typeProd (thing rl) (thing rr) (fmap thing rrs) ctx s
+-- typeOfQuery (TRef rr)        ctx s = typeRel (thing rr) ctx s
+-- typeOfQuery Empty            ctx s = 
+--   appFexpTableSch ctx $ mkOpt (F.Lit True) M.empty
 
--- | Statically type chekcs a cross product query.
---   Note that it make sures that the two types are
---   disjoint.
-typeProd :: MonadThrow m 
-         => Relation -> Relation -> [Relation] -> VariationalContext -> Schema
-         -> m TypeEnv
-typeProd l r rs ctx s = 
-  do tl <- typeRel l ctx s 
-     tr <- typeRel r ctx s 
-     ts <- mapM (flip (flip typeRel ctx) s) rs 
-     compatibleTypes $ pure tl ++ pure tr ++ ts
-     disjointTypes tl tr ts 
-     return $ prodTypes $ pure tl ++ pure tr ++ ts
+-- -- | Statically type chekcs a cross product query.
+-- --   Note that it make sures that the two types are
+-- --   disjoint.
+-- typeProd :: MonadThrow m 
+--          => Relation -> Relation -> [Relation] -> VariationalContext -> Schema
+--          -> m TypeEnv
+-- typeProd l r rs ctx s = 
+--   do tl <- typeRel l ctx s 
+--      tr <- typeRel r ctx s 
+--      ts <- mapM (flip (flip typeRel ctx) s) rs 
+--      compatibleTypes $ pure tl ++ pure tr ++ ts
+--      disjointTypes tl tr ts 
+--      return $ prodTypes $ pure tl ++ pure tr ++ ts
 
--- | Accumulates types for cross product.
-prodTypes :: [TypeEnv] -> TypeEnv
-prodTypes ts = mkOpt f r
-  where 
-    f = foldr F.And (F.Lit True) $ fmap getFexp ts
-    r = SM.unions $ fmap getObj ts
+-- -- | Accumulates types for cross product.
+-- prodTypes :: [TypeEnv] -> TypeEnv
+-- prodTypes ts = mkOpt f r
+--   where 
+--     f = foldr F.And (F.Lit True) $ fmap getFexp ts
+--     r = SM.unions $ fmap getObj ts
 
--- | Checks whether a list of type envs are disjoint or not.
-disjointTypes :: MonadThrow m 
-                 => TypeEnv -> TypeEnv -> [TypeEnv] 
-                 -> m ()
-disjointTypes l r ts 
-  | SM.keysSet (getObj l) `Set.disjoint` SM.keysSet (getObj r)
-    && disjointAll (l : r : ts) = return ()
-  | otherwise = throwM $ NotDisjointTypes (l : r : ts)
-    where
-      disjointAll (x : xs) = all (Set.disjoint (SM.keysSet (getObj x)))
-                                 (fmap (SM.keysSet . getObj) xs)
-                             && disjointAll xs
-      disjointAll [x]      = True
-      disjointAll []       = True
+-- -- | Checks whether a list of type envs are disjoint or not.
+-- disjointTypes :: MonadThrow m 
+--                  => TypeEnv -> TypeEnv -> [TypeEnv] 
+--                  -> m ()
+-- disjointTypes l r ts 
+--   | SM.keysSet (getObj l) `Set.disjoint` SM.keysSet (getObj r)
+--     && disjointAll (l : r : ts) = return ()
+--   | otherwise = throwM $ NotDisjointTypes (l : r : ts)
+--     where
+--       disjointAll (x : xs) = all (Set.disjoint (SM.keysSet (getObj x)))
+--                                  (fmap (SM.keysSet . getObj) xs)
+--                              && disjointAll xs
+--       disjointAll [x]      = True
+--       disjointAll []       = True
 
--- | Statically type checks a relation reference.
-typeRel :: MonadThrow m 
-        => Relation -> VariationalContext -> Schema
-        -> m TypeEnv
-typeRel r ctx s = 
-  do t <- lookupTableSch r s
-     appFexpTableSch ctx t
+-- -- | Statically type checks a relation reference.
+-- typeRel :: MonadThrow m 
+--         => Relation -> VariationalContext -> Schema
+--         -> m TypeEnv
+-- typeRel r ctx s = 
+--   do t <- lookupTableSch r s
+--      appFexpTableSch ctx t
 
--- | Statically type checks joins.
-typeJoin :: MonadThrow m
-         => Joins -> VariationalContext -> Schema 
-         -> m TypeEnv
-typeJoin (JoinTwoTables rl rr c) ctx s = 
-  do tl <- typeRel (thing rl) ctx s
-     tr <- typeRel (thing rr) ctx s
-     compatibleTypes $ pure tl ++ pure tr
-     let t = mkOpt (F.And (getFexp tl) (getFexp tr)) 
-                   (SM.union (getObj tl) (getObj tr))
-     typeCondition c ctx t
-     return t
-typeJoin (JoinMore js rr c)      ctx s = 
-  do tj <- typeJoin js ctx s
-     tr <- typeRel (thing rr) ctx s
-     compatibleTypes $ pure tj ++ pure tr
-     let t = mkOpt (F.And (getFexp tj) (getFexp tr))
-                   (SM.union (getObj tj) (getObj tr))
-     typeCondition c ctx t 
-     return t 
+-- -- | Statically type checks joins.
+-- typeJoin :: MonadThrow m
+--          => Joins -> VariationalContext -> Schema 
+--          -> m TypeEnv
+-- typeJoin (JoinTwoTables rl rr c) ctx s = 
+--   do tl <- typeRel (thing rl) ctx s
+--      tr <- typeRel (thing rr) ctx s
+--      compatibleTypes $ pure tl ++ pure tr
+--      let t = mkOpt (F.And (getFexp tl) (getFexp tr)) 
+--                    (SM.union (getObj tl) (getObj tr))
+--      typeCondition c ctx t
+--      return t
+-- typeJoin (JoinMore js rr c)      ctx s = 
+--   do tj <- typeJoin js ctx s
+--      tr <- typeRel (thing rr) ctx s
+--      compatibleTypes $ pure tj ++ pure tr
+--      let t = mkOpt (F.And (getFexp tj) (getFexp tr))
+--                    (SM.union (getObj tj) (getObj tr))
+--      typeCondition c ctx t 
+--      return t 
 
--- | Checks if two type envs are compatible with each other or not.
---   It assumes that the ctx has been applied to both of them already.
-compatibleTypes :: MonadThrow m 
-                => [TypeEnv] 
-                -> m ()
-compatibleTypes ts
-  | satisfiable (foldr F.And (F.Lit True) (fmap getFexp ts)) = return ()
-  | otherwise = throwM $ IncompatibleTypes ts
+-- -- | Checks if two type envs are compatible with each other or not.
+-- --   It assumes that the ctx has been applied to both of them already.
+-- compatibleTypes :: MonadThrow m 
+--                 => [TypeEnv] 
+--                 -> m ()
+-- compatibleTypes ts
+--   | satisfiable (foldr F.And (F.Lit True) (fmap getFexp ts)) = return ()
+--   | otherwise = throwM $ IncompatibleTypes ts
 
--- | Statically type checks variational sql condiitons.
-typeVsqlCond :: MonadThrow m 
-             => VsqlCond -> VariationalContext -> Schema -> TypeEnv 
-             -> m ()
-typeVsqlCond (VsqlCond c)     ctx s t = typeCondition c ctx t 
-typeVsqlCond (VsqlIn a q)     ctx s t = 
-  do t <- typeOfQuery q ctx s 
-     lookupAttFexpTypeInRowType (attribute a) (getObj t)
-     return ()
-typeVsqlCond (VsqlNot c)      ctx s t = typeVsqlCond c ctx s t 
-typeVsqlCond (VsqlOr l r)     ctx s t = 
-  do typeVsqlCond l ctx s t
-     typeVsqlCond r ctx s t 
-typeVsqlCond (VsqlAnd l r)    ctx s t = 
-  do typeVsqlCond l ctx s t
-     typeVsqlCond r ctx s t 
-typeVsqlCond (VsqlCChc f l r) ctx s t = 
-  do typeVsqlCond l (F.And ctx f) s t
-     typeVsqlCond r (F.And ctx (F.Not f)) s t
+-- -- | Statically type checks variational sql condiitons.
+-- typeVsqlCond :: MonadThrow m 
+--              => VsqlCond -> VariationalContext -> Schema -> TypeEnv 
+--              -> m ()
+-- typeVsqlCond (VsqlCond c)     ctx s t = typeCondition c ctx t 
+-- typeVsqlCond (VsqlIn a q)     ctx s t = 
+--   do t <- typeOfQuery q ctx s 
+--      lookupAttFexpTypeInRowType (attribute a) (getObj t)
+--      return ()
+-- typeVsqlCond (VsqlNot c)      ctx s t = typeVsqlCond c ctx s t 
+-- typeVsqlCond (VsqlOr l r)     ctx s t = 
+--   do typeVsqlCond l ctx s t
+--      typeVsqlCond r ctx s t 
+-- typeVsqlCond (VsqlAnd l r)    ctx s t = 
+--   do typeVsqlCond l ctx s t
+--      typeVsqlCond r ctx s t 
+-- typeVsqlCond (VsqlCChc f l r) ctx s t = 
+--   do typeVsqlCond l (F.And ctx f) s t
+--      typeVsqlCond r (F.And ctx (F.Not f)) s t
 
--- | Statically type checks variational relational conditions.
-typeCondition :: MonadThrow m 
-              => Condition -> VariationalContext -> TypeEnv
-              -> m ()
-typeCondition (Lit b)      ctx t = return ()
-typeCondition (Comp o l r) ctx t = typeComp l r t 
-typeCondition (Not c)      ctx t = typeCondition c ctx t 
-typeCondition (Or l r)     ctx t = 
-  do typeCondition l ctx t
-     typeCondition r ctx t
-typeCondition (And l r)    ctx t = 
-  do typeCondition l ctx t
-     typeCondition r ctx t
-typeCondition (CChc f l r) ctx t = 
-  do typeCondition l (F.And ctx f) t
-     typeCondition r (F.And ctx (F.Not f)) t
+-- -- | Statically type checks variational relational conditions.
+-- typeCondition :: MonadThrow m 
+--               => Condition -> VariationalContext -> TypeEnv
+--               -> m ()
+-- typeCondition (Lit b)      ctx t = return ()
+-- typeCondition (Comp o l r) ctx t = typeComp l r t 
+-- typeCondition (Not c)      ctx t = typeCondition c ctx t 
+-- typeCondition (Or l r)     ctx t = 
+--   do typeCondition l ctx t
+--      typeCondition r ctx t
+-- typeCondition (And l r)    ctx t = 
+--   do typeCondition l ctx t
+--      typeCondition r ctx t
+-- typeCondition (CChc f l r) ctx t = 
+--   do typeCondition l (F.And ctx f) t
+--      typeCondition r (F.And ctx (F.Not f)) t
 
--- | Type checks a comparison.
-typeComp :: MonadThrow m => Atom -> Atom -> TypeEnv -> m ()
-typeComp a@(Val l)  a'@(Val r)  t 
-  | typeOf l == typeOf r = return ()
-  | otherwise = throwM $ CompInvalid a a' t 
-typeComp a@(Val l)  a'@(Att r) t = 
-  do (_,at) <- lookupAttFexpTypeInRowType (attribute r) (getObj t)
-     if typeOf l == at 
-     then return () 
-     else throwM $ CompInvalid a a' t
-typeComp a@(Att l) a'@(Val r)  t = 
-  do (_,at) <- lookupAttFexpTypeInRowType (attribute l) (getObj t)
-     if typeOf r == at 
-     then return () 
-     else throwM $ CompInvalid a a' t
-typeComp a@(Att l) a'@(Att r) t = 
-  do (_,lt) <- lookupAttFexpTypeInRowType (attribute l) (getObj t)
-     (_,rt) <- lookupAttFexpTypeInRowType (attribute r) (getObj t)
-     if lt == rt
-     then return ()
-     else throwM $ CompInvalid a a' t
+-- -- | Type checks a comparison.
+-- typeComp :: MonadThrow m => Atom -> Atom -> TypeEnv -> m ()
+-- typeComp a@(Val l)  a'@(Val r)  t 
+--   | typeOf l == typeOf r = return ()
+--   | otherwise = throwM $ CompInvalid a a' t 
+-- typeComp a@(Val l)  a'@(Att r) t = 
+--   do (_,at) <- lookupAttFexpTypeInRowType (attribute r) (getObj t)
+--      if typeOf l == at 
+--      then return () 
+--      else throwM $ CompInvalid a a' t
+-- typeComp a@(Att l) a'@(Val r)  t = 
+--   do (_,at) <- lookupAttFexpTypeInRowType (attribute l) (getObj t)
+--      if typeOf r == at 
+--      then return () 
+--      else throwM $ CompInvalid a a' t
+-- typeComp a@(Att l) a'@(Att r) t = 
+--   do (_,lt) <- lookupAttFexpTypeInRowType (attribute l) (getObj t)
+--      (_,rt) <- lookupAttFexpTypeInRowType (attribute r) (getObj t)
+--      if lt == rt
+--      then return ()
+--      else throwM $ CompInvalid a a' t
 
--- | Determines the type of a projection query.
-typeProj :: MonadThrow m 
-         => OptAttributes -> Rename Algebra -> VariationalContext -> Schema
-         -> m TypeEnv
-typeProj oas rq ctx s =
-  do t' <- typeOfQuery (thing rq) ctx s 
-     if null oas 
-     then throwM $ EmptyAttrList (thing rq)
-     else do t <- typeOptAtts oas t'
-             appFexpTableSch ctx t 
+-- -- | Determines the type of a projection query.
+-- typeProj :: MonadThrow m 
+--          => OptAttributes -> Rename Algebra -> VariationalContext -> Schema
+--          -> m TypeEnv
+-- typeProj oas rq ctx s =
+--   do t' <- typeOfQuery (thing rq) ctx s 
+--      if null oas 
+--      then throwM $ EmptyAttrList (thing rq)
+--      else do t <- typeOptAtts oas t'
+--              appFexpTableSch ctx t 
 
--- | Projects a list of optional attributes from a type env.
---   it updates included attribute's pres cond by the fexp
---   assigned to them in the list. it keeps the pres cond of
---   the whole table the same as before.
-typeOptAtts :: MonadThrow m => OptAttributes -> TypeEnv -> m TypeEnv
-typeOptAtts (ora:oras) env =
-  do let a = attribute $ thing $ getObj ora 
-         fa = getFexp ora
-         newNameAtt = name $ getObj ora
-         as = getTableSchAtts env
-         fenv = getFexp env  
-         newA = case newNameAtt of
-                  Just s  -> Attribute s
-                  Nothing -> a
-     (fa',at) <- lookupAttFexpTypeInRowType a $ getObj env 
-     if F.tautImplyFexps fa (F.And fenv fa')
-     then do t <- typeOptAtts oras env
-             return $ updateOptObj 
-                       (M.union (M.singleton newA (F.And fa fa', at)) (getObj t))
-                       env
-     else throwM $ AttrNotSubsume ora env
+-- -- | Projects a list of optional attributes from a type env.
+-- --   it updates included attribute's pres cond by the fexp
+-- --   assigned to them in the list. it keeps the pres cond of
+-- --   the whole table the same as before.
+-- typeOptAtts :: MonadThrow m => OptAttributes -> TypeEnv -> m TypeEnv
+-- typeOptAtts (ora:oras) env =
+--   do let a = attribute $ thing $ getObj ora 
+--          fa = getFexp ora
+--          newNameAtt = name $ getObj ora
+--          as = getTableSchAtts env
+--          fenv = getFexp env  
+--          newA = case newNameAtt of
+--                   Just s  -> Attribute s
+--                   Nothing -> a
+--      (fa',at) <- lookupAttFexpTypeInRowType a $ getObj env 
+--      if F.tautImplyFexps fa (F.And fenv fa')
+--      then do t <- typeOptAtts oras env
+--              return $ updateOptObj 
+--                        (M.union (M.singleton newA (F.And fa fa', at)) (getObj t))
+--                        env
+--      else throwM $ AttrNotSubsume ora env
 
--- | Determines the type a set operation query.
-typeSetOp :: MonadThrow m 
-          => Algebra -> Algebra -> VariationalContext -> Schema 
-          -> m TypeEnv
-typeSetOp l r ctx s = 
-  do tl <- typeOfQuery l ctx s
-     tr <- typeOfQuery r ctx s 
-     envl <- appFexpTableSch ctx tl 
-     envr <- appFexpTableSch ctx tr
-     if typeEq envl envr
-     then return envr
-     else throwM $ NotEquiveTypeEnv envl envr ctx
+-- -- | Determines the type a set operation query.
+-- typeSetOp :: MonadThrow m 
+--           => Algebra -> Algebra -> VariationalContext -> Schema 
+--           -> m TypeEnv
+-- typeSetOp l r ctx s = 
+--   do tl <- typeOfQuery l ctx s
+--      tr <- typeOfQuery r ctx s 
+--      envl <- appFexpTableSch ctx tl 
+--      envr <- appFexpTableSch ctx tr
+--      if typeEq envl envr
+--      then return envr
+--      else throwM $ NotEquiveTypeEnv envl envr ctx
 
--- | Type enviornment equilvanecy, checks that the vCtxt are 
---   equivalent, both env have the same set of attributes,
---   and attributes fexp are equivalent
-typeEq :: TypeEnv -> TypeEnv-> Bool
-typeEq envl envr = equivalent (getFexp envl) (getFexp envr) 
-  && getRowTypeAtts (getObj envl) == getRowTypeAtts (getObj envr) 
-  && SM.isSubmapOfBy (\(o,t) (o',t') -> t == t' && equivalent o o') 
-                     (getObj envl) 
-                     (getObj envr) 
+-- -- | Type enviornment equilvanecy, checks that the vCtxt are 
+-- --   equivalent, both env have the same set of attributes,
+-- --   and attributes fexp are equivalent
+-- typeEq :: TypeEnv -> TypeEnv-> Bool
+-- typeEq envl envr = equivalent (getFexp envl) (getFexp envr) 
+--   && getRowTypeAtts (getObj envl) == getRowTypeAtts (getObj envr) 
+--   && SM.isSubmapOfBy (\(o,t) (o',t') -> t == t' && equivalent o o') 
+--                      (getObj envl) 
+--                      (getObj envr) 
 
 
 
