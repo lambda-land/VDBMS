6d3fd05569c5d8d2a769c3aebc881bc8bc332bf2
pataei <paris.ataei@gmail.com>
2019-08-05 13:23:04 -0700

    clean up

--- src/VDBMS/QueryLang/RelAlg/Relational/Algebra.hs
+++ src/VDBMS/QueryLang/RelAlg/Relational/Algebra.hs
@@ -1,9 +1,8 @@
 -- | Relational algebra.
 module VDBMS.QueryLang.RelAlg.Relational.Algebra (
 
-        RAlgebra(..),
-        -- RJoins(..),
-        SetOp(..)
+        RAlgebra(..)
+        , SetOp(..)
 
 ) where
 
@@ -16,12 +15,6 @@ import VDBMS.Variational.Opt
 import VDBMS.QueryLang.SQL.Condition
 import VDBMS.QueryLang.RelAlg.Basics.SetOp
 
--- | Conditional relational joins.
--- data RJoins 
---    = RJoinTwoTable (Rename Relation) (Rename Relation) RCondition 
---    | RJoinMore     RJoins            (Rename Relation) RCondition 
---   deriving (Data,Eq,Show,Typeable,Ord)
-
 -- | More expressive relaitonal algebra.
 --   Ie, it has renaming of attributes and subqueries in addition to 
 --   qualified attributes.
@@ -29,9 +22,7 @@ data RAlgebra
    = RSetOp SetOp RAlgebra RAlgebra
    | RProj  Attributes (Rename RAlgebra)
    | RSel   (SqlCond RAlgebra) (Rename RAlgebra)
-   -- | RJoin  RJoins
    | RJoin (Rename RAlgebra) (Rename RAlgebra) RCondition
-   -- | RProd  (Rename Relation) (Rename Relation) [Rename Relation]
    | RProd (Rename RAlgebra) (Rename RAlgebra)
    | RTRef  (Rename Relation)
    | REmpty
