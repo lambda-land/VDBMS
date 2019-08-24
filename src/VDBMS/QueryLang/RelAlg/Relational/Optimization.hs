30267fe34d3a374c6f2221dbdd56056464cd9928
pataei <paris.ataei@gmail.com>
2019-08-24 16:54:31 -0700

    rel sel dist

--- src/VDBMS/QueryLang/RelAlg/Relational/Optimization.hs
+++ src/VDBMS/QueryLang/RelAlg/Relational/Optimization.hs
@@ -4,15 +4,15 @@ module VDBMS.QueryLang.RelAlg.Relational.Optimization where
 import VDBMS.QueryLang.RelAlg.Relational.Algebra 
 -- import qualified VDBMS.Features.FeatureExpr.FeatureExpr as F
 import VDBMS.VDB.Name
--- import VDBMS.TypeSystem.Variational.TypeSystem
+import VDBMS.TypeSystem.Relational.TypeSystem
 import VDBMS.VDB.Schema.Relational.Types
 -- -- import VDBMS.Features.Config
 -- -- import VDBMS.QueryLang.ConfigQuery
 -- import VDBMS.Variational.Opt (mapFst, getObj, getFexp, applyFuncFexp, mkOpt)
 -- -- import VDBMS.Variational.Variational
 
--- import qualified Data.Map as M 
--- import qualified Data.Map.Strict as SM (lookup)
+import qualified Data.Map as M 
+import qualified Data.Map.Strict as SM (lookup)
 import Data.Maybe (catMaybes, fromJust)
 -- import Data.List (partition)
 
@@ -25,7 +25,7 @@ appOpt q s
 -- | Relational optimization rules.
 opts :: RAlgebra -> RSchema -> RAlgebra 
 opts q s = 
-  prjDistr (selDistr ((optRSel . pushOutRProj) q) s) s
+  prjDistr (selRDistr ((optRSel . pushOutRProj) q) s) s
 
 -- | Pushes out projection as far as possible.
 -- Note that you don't necessarily want to push out all projs.
@@ -86,7 +86,6 @@ relCond (SqlAnd l r)    = RAnd (relCond l) (relCond r)
 
 -- | optimizes the selection queries.
 optRSel :: RAlgebra -> RAlgebra
--- optSel = undefined
 -- σ c₁ (σ c₂ q) ≡ σ (c₁ ∧ c₂) q
 optRSel (RSel c1 (Rename Nothing (RSel c2 rq))) 
   = RSel (SqlAnd c1 c2) (renameMap optRSel rq)
@@ -114,83 +113,78 @@ optRSel (RProd rq1 rq2)   = RProd (renameMap optRSel rq1) (renameMap optRSel rq2
 optRSel q                 = q
 
 -- | selection distributive properties.
-selDistr :: RAlgebra -> RSchema -> RAlgebra
-selDistr = undefined
--- selDistr q@(Sel c1 (Rename Nothing (Join rq1 rq2 c2))) ctx s
---   -- σ c₁ (q₁ ⋈\_c₂ q₂) ≡ (σ c₁ q₁) ⋈\_c₂ q₂
---   | check c1 t1
---     = Join (Rename Nothing (Sel c1 (renameMap selDistr' rq1))) 
---            (renameMap selDistr' rq2) 
---            c2
---   -- σ c₁ (q₁ ⋈\_c₂ q₂) ≡ q₁ ⋈\_c₂ (σ c₁ q₂)
---   | check c1 t2
---     = Join (renameMap selDistr' rq1) 
---            (Rename Nothing (Sel c1 (renameMap selDistr' rq2))) 
---            c2
---   | otherwise = q 
---     where
---       selDistr' q' = selDistr q' ctx s 
---       check cond env = (not (notInCond cond) && inAttInEnv cond env)
---                     || (notInCond cond && condAttsInEnv (relCond cond) env)
---       t1 = fromJust $ typeOfQuery (thing rq1) ctx s 
---       t2 = fromJust $ typeOfQuery (thing rq2) ctx s 
--- selDistr q@(Sel (VsqlAnd c1 c2) (Rename Nothing (Join rq1 rq2 c))) ctx s 
---   -- σ (c₁ ∧ c₂) (q₁ ⋈\_c q₂) ≡ (σ c₁ q₁) ⋈\_c (σ c₂ q₂)
---   | check c1 t1 && check c2 t2 
---     = Join (Rename Nothing (Sel c1 (renameMap selDistr' rq1))) 
---            (Rename Nothing (Sel c2 (renameMap selDistr' rq2)))
---            c
---   -- σ (c₁ ∧ c₂) (q₁ ⋈\_c q₂) ≡ (σ c₂ q₁) ⋈\_c (σ c₁ q₂)
---   | check c2 t1 && check c1 t2 
---     = Join (Rename Nothing (Sel c2 (renameMap selDistr' rq1))) 
---            (Rename Nothing (Sel c1 (renameMap selDistr' rq2))) 
---            c
---     where 
---       selDistr' q' = selDistr q' ctx s 
---       check cond env = (not (notInCond cond) && inAttInEnv cond env)
---                     || (notInCond cond && condAttsInEnv (relCond cond) env)
---       t1 = fromJust $ typeOfQuery (thing rq1) ctx s 
---       t2 = fromJust $ typeOfQuery (thing rq2) ctx s 
--- selDistr (Sel c rq)       ctx s 
---   = Sel c (renameMap (flip (flip selDistr ctx) s) rq)
--- selDistr (SetOp o q1 q2)  ctx s 
---   = SetOp o (selDistr q1 ctx s ) (selDistr q2 ctx s)
--- selDistr (Proj as rq)     ctx s 
---   = Proj as (renameMap (flip (flip selDistr ctx) s) rq)
--- selDistr (AChc f q1 q2)   ctx s 
---   = AChc f (selDistr q1 ctx s) (selDistr q2 ctx s)
--- selDistr (Join rq1 rq2 c) ctx s
---   = Join (renameMap (flip (flip selDistr ctx) s) rq1)
---          (renameMap (flip (flip selDistr ctx) s) rq2)
---          c 
--- selDistr (Prod rq1 rq2)   ctx s 
---   = Prod (renameMap (flip (flip selDistr ctx) s) rq1)
---          (renameMap (flip (flip selDistr ctx) s) rq2)
--- selDistr q ctx s = q 
+selRDistr :: RAlgebra -> RSchema -> RAlgebra
+selRDistr q@(RSel c1 (Rename Nothing (RJoin rq1 rq2 c2))) s
+  -- σ c₁ (q₁ ⋈\_c₂ q₂) ≡ (σ c₁ q₁) ⋈\_c₂ q₂
+  | check c1 t1
+    = RJoin (Rename Nothing (RSel c1 (renameMap (flip selRDistr s) rq1))) 
+           (renameMap (flip selRDistr s) rq2) 
+           c2
+  -- σ c₁ (q₁ ⋈\_c₂ q₂) ≡ q₁ ⋈\_c₂ (σ c₁ q₂)
+  | check c1 t2
+    = RJoin (renameMap (flip selRDistr s) rq1) 
+           (Rename Nothing (RSel c1 (renameMap (flip selRDistr s) rq2))) 
+           c2
+  | otherwise = q 
+    where
+      check cond env = (not (notInCond cond) && inAttInEnv cond env)
+                    || (notInCond cond && condAttsInEnv (relCond cond) env)
+      t1 = fromJust $ typeOfRQuery (thing rq1) s 
+      t2 = fromJust $ typeOfRQuery (thing rq2) s 
+selRDistr q@(RSel (SqlAnd c1 c2) (Rename Nothing (RJoin rq1 rq2 c))) s 
+  -- σ (c₁ ∧ c₂) (q₁ ⋈\_c q₂) ≡ (σ c₁ q₁) ⋈\_c (σ c₂ q₂)
+  | check c1 t1 && check c2 t2 
+    = RJoin (Rename Nothing (RSel c1 (renameMap selRDistr' rq1))) 
+           (Rename Nothing (RSel c2 (renameMap selRDistr' rq2)))
+           c
+  -- σ (c₁ ∧ c₂) (q₁ ⋈\_c q₂) ≡ (σ c₂ q₁) ⋈\_c (σ c₁ q₂)
+  | check c2 t1 && check c1 t2 
+    = RJoin (Rename Nothing (RSel c2 (renameMap selRDistr' rq1))) 
+           (Rename Nothing (RSel c1 (renameMap selRDistr' rq2))) 
+           c
+    where 
+      selRDistr' = flip selRDistr s 
+      check cond env = (not (notInCond cond) && inAttInEnv cond env)
+                    || (notInCond cond && condAttsInEnv (relCond cond) env)
+      t1 = fromJust $ typeOfRQuery (thing rq1) s 
+      t2 = fromJust $ typeOfRQuery (thing rq2) s 
+selRDistr (RSel c rq)       s 
+  = RSel c (renameMap (flip selRDistr s) rq)
+selRDistr (RSetOp o q1 q2)  s 
+  = RSetOp o (selRDistr q1 s ) (selRDistr q2 s)
+selRDistr (RProj as rq)     s 
+  = RProj as (renameMap (flip selRDistr s) rq)
+selRDistr (RJoin rq1 rq2 c) s
+  = RJoin (renameMap (flip selRDistr s) rq1)
+         (renameMap (flip selRDistr s) rq2)
+         c 
+selRDistr (RProd rq1 rq2)   s 
+  = RProd (renameMap (flip selRDistr s) rq1)
+         (renameMap (flip selRDistr s) rq2)
+selRDistr q s = q 
 
 -- | gets a condition of the "IN" format and determines if
 --   its attribute exists in a type env.
--- inAttInEnv :: VsqlCond -> TypeEnv -> Bool
--- inAttInEnv (VsqlIn a q) t = attInEnv a t 
--- inAttInEnv _ _ = error "Can only accept conditions of the IN format!!"
+inAttInEnv :: SqlCond RAlgebra -> RTypeEnv -> Bool
+inAttInEnv (SqlIn a q) t = attInEnv a t 
+inAttInEnv _ _ = error "Can only accept conditions of the IN format!!"
 
 -- | checks if an attribute exists in a type env or not.
--- attInEnv :: Attr -> TypeEnv -> Bool
--- attInEnv a t = maybe False (\ _ -> True) (SM.lookup (attribute a) (getObj t))
+attInEnv :: Attr -> RTypeEnv -> Bool
+attInEnv a t = maybe False (\ _ -> True) (SM.lookup (attribute a) t)
 
 -- | takes a relational condition and determines if it's 
 --   consistent with a type env.
--- condAttsInEnv :: Condition -> TypeEnv -> Bool
--- condAttsInEnv (Lit b)        t = True
--- condAttsInEnv (Comp o a1 a2) t = case (a1, a2) of 
---   (Val v, Att a)  -> attInEnv a t 
---   (Att a, Val v)  -> attInEnv a t 
---   (Att a, Att a') -> attInEnv a t && attInEnv a' t 
---   _               -> True 
--- condAttsInEnv (Not c)        t = condAttsInEnv c t 
--- condAttsInEnv (Or c1 c2)     t = condAttsInEnv c1 t && condAttsInEnv c2 t 
--- condAttsInEnv (And c1 c2)    t = condAttsInEnv c1 t && condAttsInEnv c2 t 
--- condAttsInEnv (CChc f c1 c2) t = condAttsInEnv c1 t && condAttsInEnv c2 t
+condAttsInEnv :: RCondition -> RTypeEnv -> Bool
+condAttsInEnv (RLit b)        t = True
+condAttsInEnv (RComp o a1 a2) t = case (a1, a2) of 
+  (Val v, Att a)  -> attInEnv a t 
+  (Att a, Val v)  -> attInEnv a t 
+  (Att a, Att a') -> attInEnv a t && attInEnv a' t 
+  _               -> True 
+condAttsInEnv (RNot c)        t = condAttsInEnv c t 
+condAttsInEnv (ROr c1 c2)     t = condAttsInEnv c1 t && condAttsInEnv c2 t 
+condAttsInEnv (RAnd c1 c2)    t = condAttsInEnv c1 t && condAttsInEnv c2 t 
 
 -- | projection distributive properties.
 prjDistr :: RAlgebra -> RSchema -> RAlgebra
