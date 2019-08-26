851d774d6699001217b72cbec726762b2c560c39
pataei <paris.ataei@gmail.com>
2019-08-26 13:08:46 -0700

    add print sql

---
+++ src/VDBMS/QueryGen/MySql/PrintSql.hs
@@ -0,0 +1,38 @@
+-- | generates a sql query for values of SQL data type.
+--   the sql query is of type Doc.
+module VDBMS.QueryGen.MySql.PrintSql where 
+
+import VDBMS.QueryLang.SQL.Pure.Sql
+
+import Text.PrettyPrint
+
+-- | prints sql select queries.
+ppSql :: SqlSelect -> Doc
+ppSql (SqlSelect as ts c) = undefined
+ppSql (SqlBin o l r) = undefined
+ppSql (SqlTRef r) = undefined
+ppSql SqlEmpty = undefined
+
+-- | prints sql attribute expressions.
+ppAtts :: SqlAttrExpr -> Doc
+ppAtts SqlAllAtt = undefined
+ppAtts (SqlAttr ras) = undefined
+ppAtts (SqlNullAttr ra) = undefined
+ppAtts (SqlConcatAtt ra s) = undefined
+
+-- | prints sql relations.
+ppRel :: SqlRelation -> Doc
+ppRel (SqlSubQuery rq) = undefined
+ppRel (SqlInnerJoin l r c) = undefined
+
+-- | prints sql temporary result as CTEs.
+ppTempCTE :: SqlTempRes -> Doc
+ppTempCTE (SqlTemp rqs q) = undefined
+
+-- | prints sql temporary result as views.
+ppTempView :: SqlTempRes -> Doc
+ppTempView (SqlTemp rqs q) = undefined
+
+
+
+
