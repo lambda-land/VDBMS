717460fac3d9c44abbebf08d909f8df4704d4548
pataei <paris.ataei@gmail.com>
2019-08-27 13:28:52 -0700

    note

--- src/VDBMS/QueryGen/MySql/GenMulQsDiffSch.hs
+++ src/VDBMS/QueryGen/MySql/GenMulQsDiffSch.hs
@@ -1,5 +1,8 @@
 -- | generates a relational query with its own independent schema
 --   for each of the relational queries in a list without storing any
---   temporary result.
+--   temporary result. 
+-- Note that this approach requires us to clean up the tables we get
+-- back from the database and adjust their schema into one unified
+-- schema and accumulate all tuples into one table
 module VDBMS.QueryGen.MySql.GenMulQsDiffSch where 
 
--- src/VDBMS/QueryGen/MySql/GenMulQsDiffSchTempResStored.hs
+++ src/VDBMS/QueryGen/MySql/GenMulQsDiffSchTempResStored.hs
@@ -1,5 +1,8 @@
 -- | generates a relational query with its own independent schema
 --   for each of the relational queries in a list while taking advantage
 --   of the stored temporary results.
+-- Note that this approach requires us to clean up the tables we get
+-- back from the database and adjust their schema into one unified
+-- schema and accumulate all tuples into one table
 module VDBMS.QueryGen.MySql.GenMulQsDiffSchTempResStored where 
 
