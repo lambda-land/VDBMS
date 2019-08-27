686f8265fea8461a384466723c1c111ef30c8592
pataei <paris.ataei@gmail.com>
2019-08-27 16:58:22 -0700

    undef adjustqsch

--- src/VDBMS/QueryGen/MySql/GenMulQsSameSch.hs
+++ src/VDBMS/QueryGen/MySql/GenMulQsSameSch.hs
@@ -7,16 +7,22 @@ import VDBMS.QueryLang.RelAlg.Relational.Algebra (RAlgebra)
 import VDBMS.QueryLang.SQL.Pure.Sql (SqlSelect)
 import VDBMS.VDB.Schema.Variational.Schema (TableSchema, getTableSchAttsList)
 import VDBMS.VDB.Schema.Relational.Types (RSchema)
+import VDBMS.TypeSystem.Relational.TypeSystem (typeOfRQuery)
+import VDBMS.VDB.Name (Attribute)
+import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
 
 import Control.Monad.Catch 
 
 -- | generates sql queries for relational queries based on
 --   a given variational table schema. 
-genQsSameSch :: MonadThrow m 
-  => TableSchema -> [(RAlgebra, RSchema)] 
-  -> [m SqlSelect]
-genQsSameSch t qss =
-  do let as = getTableSchAttsList t 
-     ts <- mapM (uncurry typeOfRQuery) qss 
-     return undefined
+genQsSameSch :: TableSchema -> [RAlgebra] -> [SqlSelect]
+genQsSameSch t qs = fmap ((adjustQSch as) . transAlgebra2Sql) qs
+  where
+    as = getTableSchAttsList t 
+     -- ts <- mapM (uncurry typeOfRQuery) qss 
+     -- let qs = fmap ((adjustQSch as) . transAlgebra2Sql . fst) qss
+     -- return undefined
 
+-- | adjusts the schema of a sql query wrt a given list of attribute.
+adjustQSch :: [Attribute] -> SqlSelect -> SqlSelect
+adjustQSch = undefined
