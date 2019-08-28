c21a8d553d706e981334a38f646f8b9bc030b059
pataei <paris.ataei@gmail.com>
2019-08-27 17:29:39 -0700

    without temp res done ... have to fill out three undefined in sql ops and vsch core

--- src/VDBMS/QueryGen/MySql/GenMulQsSameSch.hs
+++ src/VDBMS/QueryGen/MySql/GenMulQsSameSch.hs
@@ -1,13 +1,17 @@
 -- | generates a relational query with the same schema
 --   for each of the relational queries in a list without storing any
 --   temporary result.
-module VDBMS.QueryGen.MySql.GenMulQsSameSch where 
+module VDBMS.QueryGen.MySql.GenMulQsSameSch (
+
+       genQsSameSch
+
+) where 
 
 import VDBMS.QueryLang.RelAlg.Relational.Algebra (RAlgebra)
 import VDBMS.QueryLang.SQL.Pure.Sql (SqlSelect)
 import VDBMS.VDB.Schema.Variational.Schema (TableSchema, getTableSchAttsList)
 import VDBMS.VDB.Schema.Relational.Types (RSchema)
-import VDBMS.TypeSystem.Relational.TypeSystem (typeOfRQuery)
+-- import VDBMS.TypeSystem.Relational.TypeSystem (typeOfRQuery)
 import VDBMS.VDB.Name (Attribute)
 import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
 import VDBMS.QueryLang.SQL.Pure.Ops
--- src/VDBMS/QueryGen/MySql/GenOneQ.hs
+++ src/VDBMS/QueryGen/MySql/GenOneQ.hs
@@ -1,3 +1,16 @@
 -- | generates one relational query for a given variational query without 
 --   storing any temporary results.
 module VDBMS.QueryGen.MySql.GenOneQ where 
+
+import VDBMS.QueryGen.MySql.GenMulQsSameSch
+import VDBMS.QueryLang.RelAlg.Relational.Algebra (RAlgebra)
+import VDBMS.QueryLang.SQL.Pure.Sql 
+import VDBMS.VDB.Schema.Variational.Schema (TableSchema)
+import VDBMS.VDB.Schema.Relational.Types (RSchema)
+
+
+-- | generates a query from all queries generated with a same schema.
+genQ :: TableSchema -> [(RAlgebra, RSchema)] -> SqlSelect
+genQ t qss = foldr  (SqlBin SqlUnionAll) (head qs) (tail qs) 
+  where 
+    qs = genQsSameSch t qss 
