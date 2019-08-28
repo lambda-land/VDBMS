63cd077506e381f1c5071f9a31b3b827216a7763
pataei <paris.ataei@gmail.com>
2019-08-28 16:36:03 -0700

    clean up

--- src/VDBMS/QueryGen/MySql/GenMulQsSameSch.hs
+++ src/VDBMS/QueryGen/MySql/GenMulQsSameSch.hs
@@ -16,22 +16,12 @@ import VDBMS.VDB.Name (Attribute)
 import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
 import VDBMS.QueryLang.SQL.Pure.Ops
 
--- import Control.Monad.Catch 
 import Control.Arrow ((***))
 
--- | generates sql queries for relational queries based on
---   a given variational table schema. 
+-- | generates sql queries for relational queries passed
+--   with their type env based on
+--   a given list of attribute.
 genQsSameSch :: [Attribute] -> [(RAlgebra, RTypeEnv)] -> [SqlSelect]
 genQsSameSch as qts =
   fmap (\(q,atts) -> adjustQSch as atts q) 
        (fmap (transAlgebra2Sql *** rtypeEnvAtts) qts)
-       -- (fmap (\(r,t) -> (transAlgebra2Sql r, rtypeEnvAtts t)) qts)
-  -- do ts <- mapM (uncurry typeOfRQuery) qss
-  --    qs
-  -- = fmap (\(q,r) -> (adjustQSch as (transAlgebra2Sql q) r)) qss
-  -- where
-  --   as = getTableSchAttsList t 
-     -- ts <- mapM (uncurry typeOfRQuery) qss 
-     -- let qs = fmap ((adjustQSch as) . transAlgebra2Sql . fst) qss
-     -- return undefined
-
--- src/VDBMS/QueryGen/MySql/GenOneQ.hs
+++ src/VDBMS/QueryGen/MySql/GenOneQ.hs
@@ -4,12 +4,13 @@ module VDBMS.QueryGen.MySql.GenOneQ where
 
 import VDBMS.QueryGen.MySql.GenMulQsSameSch
 import VDBMS.QueryLang.RelAlg.Relational.Algebra (RAlgebra)
-import VDBMS.QueryLang.SQL.Pure.Sql 
+import VDBMS.QueryLang.SQL.Pure.Sql (SqlSelect)
 import VDBMS.VDB.Name (Attribute)
 import VDBMS.TypeSystem.Relational.TypeSystem (RTypeEnv)
 
 
--- | generates a query from all queries generated with a same schema.
+-- | generates a query from all queries generated with a same 
+--   list of attribute.
 genQ :: [Attribute] -> [(RAlgebra, RTypeEnv)] -> SqlSelect
 genQ t qss = foldr  (SqlBin SqlUnionAll) (head qs) (tail qs) 
   where 
