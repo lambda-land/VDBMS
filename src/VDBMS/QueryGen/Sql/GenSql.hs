6e54e4c3aa7c7cfabe091f0e5780755afb439d8e
pataei <paris.ataei@gmail.com>
2019-09-01 14:15:37 -0700

    add gen sql

---
+++ src/VDBMS/QueryGen/Sql/GenSql.hs
@@ -0,0 +1,27 @@
+-- | tranlates relational queries to sql.
+module VDBMS.QueryGen.Sql.GenSql (
+
+        genSql
+
+) where 
+
+import VDBMS.QueryLang.RelAlg.Relational.Algebra 
+import VDBMS.QueryLang.SQL.Pure.Sql
+-- import VDBMS.VDB.Name 
+-- import VDBMS.QueryLang.SQL.Condition 
+-- import VDBMS.QueryLang.RelAlg.Basics.SetOp
+-- import VDBMS.VDB.Schema.Variational.Schema
+
+-- import Data.List ((\\))
+
+-- type FreshName = Int 
+
+-- -- | A Query monad provides unique names (aliases)
+-- --   and constructs a SqlSelect.
+-- type QState = (SqlSelect, FreshName)
+-- data Query a = Query (QState -> (a, QState))
+
+
+-- 
+genSql :: RAlgebra -> SqlSelect
+genSql = undefined
