e6ddea425a746d92d8fe056f33f8da66d4446253
pataei <paris.ataei@gmail.com>
2019-09-01 15:16:01 -0700

    removed warnings

--- src/VDBMS/VDB/Schema/Relational/Lookups.hs
+++ src/VDBMS/VDB/Schema/Relational/Lookups.hs
@@ -7,7 +7,7 @@ module VDBMS.VDB.Schema.Relational.Lookups (
 
 ) where
 
-import Data.Map.Strict (Map)
+-- import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 
 import Control.Monad.Catch
@@ -16,11 +16,14 @@ import VDBMS.VDB.Schema.Relational.Types
 import VDBMS.VDB.Name
 import VDBMS.DBMS.Value.Type (SqlType)
 
+-- | lookups the table schema of a relation from a schema.
 rlookupRelation :: MonadThrow m => Relation -> RSchema -> m RTableSchema
 rlookupRelation r s = maybe (throwM $ RMissingRelation r) return $ M.lookup r s
 
+-- | lookups the sqltype of an attribute from a table schema.
 rlookupAttrType :: MonadThrow m => Attribute -> RTableSchema -> m SqlType
 rlookupAttrType a t = maybe (throwM $ RMissingAttribute a) return $ M.lookup a t
 
+-- | lookups the attribute list of a relation from a schema.
 rlookupAttsList :: MonadThrow m => Relation -> RSchema -> m [Attribute]
 rlookupAttsList r s = M.keys <$> rlookupRelation r s
