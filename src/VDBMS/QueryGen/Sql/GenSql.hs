53b502ec5fe6bb4f35eb8e1a97969be2b6d1d640
pataei <paris.ataei@gmail.com>
2019-09-25 14:56:12 -0700

    name subq unerway

--- src/VDBMS/QueryGen/Sql/GenSql.hs
+++ src/VDBMS/QueryGen/Sql/GenSql.hs
@@ -1,12 +1,14 @@
 -- | tranlates relational queries to sql.
 module VDBMS.QueryGen.Sql.GenSql (
 
-        genSql
+         genSql
+       , nameSubSql
 
 ) where 
 
 import VDBMS.QueryLang.RelAlg.Relational.Algebra 
 import VDBMS.QueryLang.SQL.Pure.Sql
+import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
 -- import VDBMS.VDB.Name 
 -- import VDBMS.QueryLang.SQL.Condition 
 -- import VDBMS.QueryLang.RelAlg.Basics.SetOp
@@ -21,6 +23,19 @@ type FreshName = Int
 type QState = (SqlSelect, FreshName)
 data Query a = Query (QState -> (a, QState))
 
--- 
+-- | generates a sql query from a RA query while creating a 
+--   new name for subqueries and keeping the names that 
+--   the user has used in their original query.
 genSql :: RAlgebra -> SqlSelect
-genSql = undefined
+genSql = nameSubSql . transAlgebra2Sql 
+
+-- | names subqueries within a sql query.
+nameSubSql :: SqlSelect -> SqlSelect
+nameSubSql (SqlSelect as ts cs) = SqlSelect as (nameRels ts) cs
+nameSubSql q = q
+
+-- | 
+nameRels :: [SqlRelation] -> [SqlRelation]
+nameRels = undefined
+
+-- nameRel :: SqlRelation -> 
