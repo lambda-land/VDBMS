aecf5401d5358f3229b5b9c9c9675fec8ee6d070
pataei <paris.ataei@gmail.com>
2019-09-26 13:39:10 -0700

    add gen sql ctes

---
+++ src/VDBMS/QueryGen/Sql/GenSqlWithCTE.hs
@@ -0,0 +1,21 @@
+-- | generates sql queries with ctes given a sql query.
+module VDBMS.QueryGen.Sql.GenSqlWithCTE (
+
+         genSqlCTE
+
+) where 
+
+-- import VDBMS.QueryLang.RelAlg.Relational.Algebra 
+import VDBMS.QueryLang.SQL.Pure.Sql
+-- import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
+-- import VDBMS.VDB.Name (Rename(..))
+-- import VDBMS.QueryLang.SQL.Condition 
+-- import VDBMS.QueryLang.RelAlg.Basics.SetOp
+-- import VDBMS.VDB.Schema.Variational.Schema
+
+-- import Data.List ((\\))
+
+-- import Control.Monad.State 
+
+genSqlCTE :: SqlSelect -> SqlSelect
+genSqlCTE = undefined
