479e7ee8b068e6b1b5922dc1dfee773a1a827213
QiaoranChelsea <liqiao@oregonstate.edu>
2019-10-23 15:57:03 -0700

    steps taken to build employee vdb

--- databases/employeeDB/step1_chop_employees.sql
+++ databases/employeeDB/step1_chop_employees.sql
@@ -1,4 +1,25 @@
--- SELECT count(emp.emp_no) -- 152216
+# create a view for engineerpersonnel in version 1
+# SELECT count(emp.emp_no) -- 42247 
+Create view v1_engineerpersonnel_view AS 
+SELECT emp.emp_no as empno, concat(first_name, " ", last_name) as name, hire_date as hiredate, title, dept_name as deptname
+FROM employees emp 
+JOIN dept_emp de on emp.emp_no = de.emp_no 
+JOIN departments dept on dept.dept_no = de.dept_no
+JOIN titles on titles.emp_no = emp.emp_no
+WHERE hire_date < '1988-01-01' and title in ('Engineer', 'Senior Engineer', 'Assistant Engineer') and de.to_date = '9999-01-01' and titles.to_date =  '9999-01-01' ;
+
+# create a view for otherpersonnel in version 1
+# SELECT count(emp.emp_no) -- 41568 
+CREATE view v1_otherpersonnel_view AS 
+SELECT emp.emp_no as empno, concat(first_name, " ", last_name) as name, hire_date as hiredate, title, dept_name as deptname
+FROM employees emp 
+JOIN dept_emp de on emp.emp_no = de.emp_no 
+JOIN departments dept on dept.dept_no = de.dept_no
+JOIN titles on titles.emp_no = emp.emp_no
+WHERE hire_date < '1988-01-01' and title not in ('Engineer', 'Senior Engineer', 'Assistant Engineer') and de.to_date = '9999-01-01' and titles.to_date =  '9999-01-01' ;
+
+# create a view for empacct of version 2 : 
+# SELECT count(emp.emp_no) -- 152216
 Create OR REPLACE view v2_empacct_view AS 
 SELECT emp.emp_no as empno, concat(first_name, " ", last_name) as name, hire_date as hiredate, title, dept_name as deptname
 FROM employees emp 
@@ -7,7 +28,8 @@ JOIN departments dept on dept.dept_no = de.dept_no
 JOIN titles on titles.emp_no = emp.emp_no
 WHERE hire_date < '1991-01-01' and de.to_date = '9999-01-01'  and titles.to_date = '9999-01-01';
 
--- SELECT count(emp.emp_no) -- 201006
+# create a view for empacct of version 3
+-- # SELECT count(emp.emp_no) -- 201006
 Create OR REPLACE view v3_empacct_view AS 
 SELECT emp.emp_no as empno, concat(first_name, " ", last_name) as name, hire_date as hiredate, title, dept.dept_no as deptno
 FROM employees emp 
@@ -16,7 +38,8 @@ JOIN departments dept on dept.dept_no = de.dept_no
 JOIN titles on titles.emp_no = emp.emp_no
 WHERE  hire_date < '1994-01-01' and de.to_date = '9999-01-01'  and titles.to_date = '9999-01-01';
 
--- SELECT count(emp.emp_no) -- 230297 
+# create a view for empacct of version 4
+-- # SELECT count(emp.emp_no) -- 230297 
 Create OR REPLACE view v4_empacct_view AS 
 SELECT emp.emp_no as empno, hire_date as hiredate, title, dept.dept_no as deptno
 FROM employees emp 
@@ -25,17 +48,8 @@ JOIN departments dept on dept.dept_no = de.dept_no
 JOIN titles on titles.emp_no = emp.emp_no
 WHERE hire_date < '1997-01-01' and de.to_date = '9999-01-01'  and titles.to_date = '9999-01-01';
 
--- SELECT count(emp.emp_no) -- 29291 -- 37286 without title.to_date = .... 
-Create OR REPLACE view v4_empbio_view AS 
-SELECT emp.emp_no as empno, gender as sex, birth_date as birthdate, concat(first_name, " ", last_name) as name
-FROM employees emp 
-JOIN dept_emp de on emp.emp_no = de.emp_no 
-JOIN departments dept on dept.dept_no = de.dept_no
-JOIN titles on titles.emp_no = emp.emp_no
-WHERE hire_date < '1997-01-01' and de.to_date = '9999-01-01'  and titles.to_date = '9999-01-01'
-order by emp.emp_no;
-
--- SELECT count(emp.emp_no) -- 240124
+# create a view for empacct of version 5
+# SELECT count(emp.emp_no) -- 240124
 Create OR REPLACE view v5_empacct_view AS 
 SELECT emp.emp_no as empno, hire_date as hiredate, title, dept.dept_no as deptno, salary
 FROM employees emp 
@@ -45,7 +59,19 @@ JOIN salaries on emp.emp_no = salaries.emp_no
 JOIN titles on titles.emp_no = emp.emp_no
 WHERE hire_date < '2000-01-28' and de.to_date = '9999-01-01' and salaries.to_date = '9999-01-01';
 
--- SELECT count( emp.emp_no) -- 240124
+# create a view for empbio of version 4
+# SELECT count(emp.emp_no) -- 29291 -- 37286 without title.to_date = .... 
+Create OR REPLACE view v4_empbio_view AS 
+SELECT emp.emp_no as empno, gender as sex, birth_date as birthdate, concat(first_name, " ", last_name) as name
+FROM employees emp 
+JOIN dept_emp de on emp.emp_no = de.emp_no 
+JOIN departments dept on dept.dept_no = de.dept_no
+JOIN titles on titles.emp_no = emp.emp_no
+WHERE hire_date < '1997-01-01' and de.to_date = '9999-01-01'  and titles.to_date = '9999-01-01'
+order by emp.emp_no;
+
+# create a view for empbio of version 5
+# SELECT count( emp.emp_no) -- 240124
 Create OR REPLACE view v5_empbio_view AS 
 SELECT emp.emp_no as empno, gender as sex, birth_date as birthdate, first_name as  firstname, last_name as lastname
 FROM employees emp 
@@ -55,6 +81,7 @@ JOIN titles on titles.emp_no = emp.emp_no
 WHERE hire_date < '2000-01-28'and salaries.to_date = '9999-01-01' and de.to_date = '9999-01-01' and titles.to_date =  '9999-01-01' 
 order by emp.emp_no;
 
+# create a view for dept of version 3
 Create OR REPLACE view v3_dept_view AS 
 SELECT dept_name as deptname, dept.dept_no as deptno, dept_manager.emp_no as managerno
 FROM dept_manager  
@@ -62,6 +89,7 @@ JOIN departments dept on dept.dept_no = dept_manager.dept_no
 JOIN employees emp on emp.emp_no = dept_manager.emp_no
 WHERE hire_date < '1994-01-01'  and dept_manager.to_date = '9999-01-01';
 
+# create a view for dept of version 4
 Create OR REPLACE view v4_dept_view  AS 
 SELECT dept_name as deptname, dept.dept_no as deptno, dept_manager.emp_no as managerno
 FROM dept_manager  
@@ -69,11 +97,21 @@ JOIN departments dept on dept.dept_no = dept_manager.dept_no
 JOIN employees emp on emp.emp_no = dept_manager.emp_no
 WHERE hire_date < '1997-01-01'  and dept_manager.to_date = '9999-01-01';
 
+# create a view for dept of version 5
 Create OR REPLACE view v5_dept AS 
 SELECT dept_name as deptname, dept.dept_no as deptno, dept_manager.emp_no as managerno
 FROM dept_manager  
 JOIN departments dept on dept.dept_no = dept_manager.dept_no
 JOIN employees emp on emp.emp_no = dept_manager.emp_no
 WHERE hire_date < '2000-01-28'  and dept_manager.to_date = '9999-01-01';
+
+# create a view for job
+Create OR REPLACE view job_view AS 
+SELECT title, salary 
+FROM employees emp 
+JOIN salaries on emp.emp_no = salaries.emp_no
+JOIN titles on titles.emp_no = emp.emp_no
+WHERE hire_date < '2000-01-28' and salaries.to_date = '9999-01-01' and titles.to_date =  '9999-01-01';
+
 
 flush /*!50503 binary */ logs;
--- databases/employeeDB/step2_build_vdb.sql
+++ databases/employeeDB/step2_build_vdb.sql
@@ -1,18 +1,18 @@
 # v_engineerpersonnel(empno, name, hiredate, title, deptname, presCond)
--- DROP TABLE IF EXISTS `v_engineerpersonnel`;
--- CREATE TABLE `v_engineerpersonnel` (
---   `empno` int(11) NOT NULL,
---   `name` varchar(31) DEFAULT NULL,
---   `hiredate` date NOT NULL,
---   `title` varchar(50) NOT NULL,
---   `deptname` varchar(40) NOT NULL,
---   `presCond` varchar(31) DEFAULT NULL
--- );
+DROP TABLE IF EXISTS `v_engineerpersonnel`;
+CREATE TABLE `v_engineerpersonnel` (
+  `empno` int(11) NOT NULL,
+  `name` varchar(31) DEFAULT NULL,
+  `hiredate` date NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `deptname` varchar(40) NOT NULL,
+  `presCond` varchar(31) DEFAULT NULL
+);
 
--- INSERT INTO v_engineerpersonnel (empno, name, hiredate,title, deptname, presCond)
--- SELECT empno, name,hiredate,title, deptname, "v1" FROM v1_engineerpersonnel_view;
+INSERT INTO v_engineerpersonnel (empno, name, hiredate,title, deptname, presCond)
+SELECT empno, name,hiredate,title, deptname, "v1" FROM v1_engineerpersonnel_view;
 
--- # v_otherpersonnel (empno, name, hiredate, title, deptname, presCond)
+# v_otherpersonnel (empno, name, hiredate, title, deptname, presCond)
 DROP TABLE IF EXISTS `v_otherpersonnel`;
 CREATE TABLE `v_otherpersonnel` (
   `empno` int(11) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE `v_otherpersonnel` (
 INSERT INTO v_otherpersonnel (empno, name, hiredate,title, deptname, presCond)
 SELECT empno, name,hiredate,title, deptname, "v1" FROM v1_otherpersonnel_view;
 
--- # v_empacct(empno, name, hiredate, title, deptname, deptno, salary, presCond)
+# v_empacct(empno, name, hiredate, title, deptname, deptno, salary, presCond)
 DROP TABLE IF EXISTS `v_empacct`;
 CREATE TABLE `v_empacct` (
   `empno` int(11) NOT NULL,
@@ -55,14 +55,16 @@ INSERT INTO v_empacct(empno, name, hiredate, title, deptname, deptno, salary, pr
 SELECT empno, NULL, hiredate, title, NULL, deptno, salary, "v5" 
 FROM v5_empacct_view;
 
--- # v_job(title, salary, presCond)
--- DROP TABLE IF EXISTS `v_job`;
--- CREATE TABLE `v_job` (
---   `title` varchar(50) NOT NULL,
---   `salary` int(11) DEFAULT NULL,
---   `presCond` varchar(31) NOT NULL
--- ) 
+# v_job(title, salary, presCond)
+DROP TABLE IF EXISTS `v_job`;
+CREATE TABLE `v_job` (
+  `title` varchar(50) NOT NULL,
+  `salary` int(11) DEFAULT NULL,
+  `presCond` varchar(31) NOT NULL
+) ;
 
+INSERT INTO v_job(title, salary, presCond)
+SELECT title, salary,"True" FROM job_view;
 
 # v_dept(deptname, deptno, managerno,presCond)
 DROP TABLE IF EXISTS `v_dept`;
@@ -82,7 +84,7 @@ SELECT deptname, deptno, managerno,"v4" FROM v4_dept_view;
 INSERT INTO v_dept(deptname, deptno, managerno,presCond)
 SELECT deptname, deptno, managerno,"v5" FROM v5_dept_view;
 
--- # v_empbio(empno, sex, birthdate, name, firstname, lastname presCond)
+# v_empbio(empno, sex, birthdate, name, firstname, lastname presCond)
 DROP TABLE IF EXISTS `v_empbio`;
 CREATE TABLE `v_empbio` (
   `empno` int(11) NOT NULL,
