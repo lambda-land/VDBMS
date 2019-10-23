5db4624508f21413c6d4003ea55d29a9a2fac4d1
pataei <paris.ataei@gmail.com>
2019-10-23 11:47:48 -0700

    two pat left

--- src/VDBMS/QueryGen/VRA/PushSchToQ.hs
+++ src/VDBMS/QueryGen/VRA/PushSchToQ.hs
@@ -18,16 +18,26 @@ import VDBMS.VDB.GenName
 pushSchToQ :: Schema -> Algebra -> Algebra
 pushSchToQ s (SetOp o l r) = undefined
 pushSchToQ s (Proj as rq) = undefined
-pushSchToQ s (Sel c rq) = undefined
-pushSchToQ s (AChc f l r) = undefined
-pushSchToQ s (Join rl rr c) = undefined
+pushSchToQ s (Sel c rq) 
+  = Sel (pushSchToCond s c) (renameMap (pushSchToQ s) rq)
+pushSchToQ s (AChc f l r) 
+  = AChc f (pushSchToQ s l) (pushSchToQ s r)
+pushSchToQ s (Join rl rr c) 
+  = Join (renameMap (pushSchToQ s) rl) (renameMap (pushSchToQ s) rr) c
 pushSchToQ s (Prod rl rr) 
-  = undefined
+  = Prod (renameMap (pushSchToQ s) rl) (renameMap (pushSchToQ s) rr)
 pushSchToQ s q@(TRef rr) 
   = Proj (relSchToOptAtts (thing rr) s) (renameNothing q)
 pushSchToQ s Empty = Empty
 
 
--- | 
+-- | takes a relation and schema and generates
+--   the list of optattributes of the relationschema
+--   in the schema.
 relSchToOptAtts :: Relation -> Schema -> OptAttributes
 relSchToOptAtts = undefined
+
+-- | pushes schema to vsqlcond.
+pushSchToCond :: Schema -> VsqlCond -> VsqlCond
+pushSchToCond = undefined
+
