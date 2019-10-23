d60638aa967380775df8c5e788642dcd6e75ad6a
pataei <paris.ataei@gmail.com>
2019-10-23 12:20:59 -0700

    two helper and prj ...

--- src/VDBMS/QueryGen/VRA/PushSchToQ.hs
+++ src/VDBMS/QueryGen/VRA/PushSchToQ.hs
@@ -19,6 +19,7 @@ pushSchToQ :: Schema -> Algebra -> Algebra
 pushSchToQ s (SetOp o l r) 
   = SetOp o (pushSchToQ s l) (pushSchToQ s r) 
 pushSchToQ s (Proj as rq) = undefined
+  -- = Proj () (renameMap (pushSchToQ s) rq)
 pushSchToQ s (Sel c rq) 
   = Sel (pushSchToCond s c) (renameMap (pushSchToQ s) rq)
 pushSchToQ s (AChc f l r) 
@@ -34,15 +35,19 @@ pushSchToQ s Empty = Empty
 
 -- | takes a relation and schema and generates
 --   the list of optattributes of the relationschema
---   in the schema.
+--   in the schema. 
 relSchToOptAtts :: Relation -> Schema -> OptAttributes
 relSchToOptAtts = undefined
+
+-- | 
+pushSchToOptAtts :: Schema -> OptAttributes -> OptAttributes
+pushSchToOptAtts = undefined
 
 -- | pushes schema to vsqlcond.
 pushSchToCond :: Schema -> VsqlCond -> VsqlCond
 pushSchToCond _ cnd@(VsqlCond _) = cnd
-pushSchToCond s (VsqlIn a q) = VsqlIn a (pushSchToQ s q)
-pushSchToCond s (VsqlNot c)    = VsqlNot (pushSchToCond s c)
+pushSchToCond s (VsqlIn a q)     = VsqlIn a (pushSchToQ s q)
+pushSchToCond s (VsqlNot c)      = VsqlNot (pushSchToCond s c)
 pushSchToCond s (VsqlOr l r) 
   = VsqlOr (pushSchToCond s l) (pushSchToCond s r)
 pushSchToCond s (VsqlAnd l r) 
