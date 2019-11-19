b3cb5e33db44c7e93525315b2572b777a9342a15
pataei <paris.ataei@gmail.com>
2019-11-19 12:48:24 -0800

    sat fm

--- src/VDBMS/VDB/Schema/Variational/Tests.hs
+++ src/VDBMS/VDB/Schema/Variational/Tests.hs
@@ -7,15 +7,32 @@ module VDBMS.VDB.Schema.Variational.Tests (
 
 import VDBMS.Features.SAT
 import VDBMS.VDB.Schema.Variational.Schema
+import VDBMS.Features.FeatureExpr.FeatureExpr
+import VDBMS.VDB.Name 
+
+import Control.Monad.Catch 
+import Data.Data (Data, Typeable)
+import GHC.Generics (Generic)
+
+-- | Errors for schema validity checks.
+data SchValErr 
+  = FMunsat FeatureExpr
+  | RelPCunsat Relation FeatureExpr
+  | AttPCunsat Attribute FeatureExpr
+  deriving (Data,Eq,Generic,Ord,Show,Typeable)
+
+instance Exception SchValErr
 
 -- | checks 1) if fm is sat, 2) if all relations
 --   pc are sat, and 3) if all atts pc are sat.
-isVschValid :: Schema -> Bool 
-isVschValid = undefined
+isVschValid :: MonadThrow m => Schema -> m Bool 
+isVschValid s = undefined
 
 -- | checks if the feature model is satisfiable. 
-satFM :: Schema -> Bool
-satFM = satisfiable . featureModel
+satFM :: MonadThrow m => Schema -> m Bool 
+satFM s
+  | (satisfiable . featureModel) s == True = return True
+  | otherwise = throwM $ FMunsat (featureModel s)
 
 -- | checks if a relation pc is satisfiable. 
 -- satRPC :: Schema -> Bool 
