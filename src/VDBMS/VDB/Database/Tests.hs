e6a6aae5078edbe6888a26a0785a116db5fd0b08
pataei <paris.ataei@gmail.com>
2019-11-19 13:53:57 -0800

    add vdb tests

---
+++ src/VDBMS/VDB/Database/Tests.hs
@@ -0,0 +1,9 @@
+-- | Database tests.
+module VDBMS.VDB.Database.Tests where
+
+-- import VDBMS.Features.Config
+-- import VDBMS.Features.ConfFexp
+-- import VDBMS.VDB.Schema.Variational.Schema
+-- import VDBMS.VDB.Table.Table
+-- import VDBMS.VDB.Name (PCatt)
+
