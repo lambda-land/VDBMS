8a1a5ac6e0048d9d0c989566a1de20605a818c8e
pataei <paris.ataei@gmail.com>
2019-12-11 17:24:29 -0800

    add fetch

---
+++ src/VDBMS/VDB/Database/HDBC/Fetch.hs
@@ -0,0 +1,19 @@
+-- | HDBC library fetching result as Table.
+module VDBMS.VDB.Database.HDBC.Fetch where
+
+import VDBMS.VDB.Schema.Variational.Schema
+import VDBMS.VDB.Name
+import VDBMS.VDB.Database.Database
+import VDBMS.VDB.Table.Table
+
+import Database.HDBC 
+
+
+-- | fetchs result of a query in a table for any 
+--   instance of IConnection.
+fetch :: IConnection conn => conn -> Query -> IO Table
+fetch = undefined
+
+-- | stirct version of fetch.
+fetch' :: IConnection conn => conn -> Query -> IO Table
+fetch' = undefined
