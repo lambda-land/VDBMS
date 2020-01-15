b1849051d9aa4fc411432baded6c4fed5fa6fbe8
QiaoranChelsea <liqiao@oregonstate.edu>
2020-01-15 22:11:23 +0800

    5 pure schema variants

--- src/VDBMS/UseCases/EnronUseCase/EnronSchema.hs
+++ src/VDBMS/UseCases/EnronUseCase/EnronSchema.hs
@@ -6,6 +6,7 @@ import qualified VDBMS.VDB.Name as N
 import VDBMS.VDB.Schema.Variational.Schema
 import VDBMS.DBMS.Value.Value
 import VDBMS.UseCases.SmartConstructor
+import VDBMS.VDB.Schema.Relational.Types
 
 -- 
 --  * Features
@@ -75,6 +76,39 @@ pseudonym, suffix :: N.Attr
 pseudonym = attr "pseudonym"
 suffix = attr "suffix"
 
+eid_, firstname_, lastname_, email_id_, folder_, status_, verification_key_, puclic_key_ :: N.Attribute 
+eid_ = N.Attribute "eid"
+firstname_ = N.Attribute "firstname"
+lastname_ = N.Attribute "lastname"
+email_id_ = N.Attribute "email_id"
+folder_ = N.Attribute "folder"
+status_ = N.Attribute "status"
+verification_key_ = N.Attribute "verification_key"
+puclic_key_ = N.Attribute "puclic_key"
+
+mid_, sender_, date_, message_id_, subject_, body_, is_signed_, is_encrypted_ :: N.Attribute 
+is_autoresponse_, is_forward_msg_, is_system_notification_:: N.Attribute 
+mid_ = N.Attribute "mid"
+sender_ = N.Attribute "sender"
+date_ = N.Attribute "date"
+message_id_ = N.Attribute "message_id"
+subject_ = N.Attribute "subject"
+body_    = N.Attribute "body"
+is_signed_ = N.Attribute "is_signed"
+is_encrypted_ = N.Attribute "is_encrypted"
+is_autoresponse_ = N.Attribute "is_autoresponse"
+is_forward_msg_ = N.Attribute "is_forward_msg"
+is_system_notification_ = N.Attribute "is_system_notification"
+
+rid_, rfid_, rtype_, rvalue_, reference_, suffix_, forwardaddr_, pseudonym_:: N.Attribute
+rid_ = N.Attribute "rid"  
+rfid_ = N.Attribute "rfid"
+rtype_ = N.Attribute "rtype"
+rvalue_ = N.Attribute "rvalue"
+reference_ = N.Attribute "reference"
+suffix_ = N.Attribute "suffix"
+forwardaddr_ = N.Attribute "forwardaddr"
+pseudonym_ = N.Attribute "pseudonym"
 --
 -- * Enron VDB Feature Model
 --
@@ -99,28 +133,28 @@ enronVSchema = (enronFeatureModel, constructOptRelMap [ (Lit True, employeelist,
 
 employeelist_vrelation, messages_vrelation, recipientinfo_vrelation, referenceinfo_vrelation, auto_msg_vrelation :: [(FeatureExpr, N.Attribute, SqlType)] 
 forward_msg_vrelation, remail_msg_vrelation, filter_msg_vrelation, mail_host_vrelation, alias_vrelation :: [(FeatureExpr, N.Attribute, SqlType)] 
-employeelist_vrelation = [ (Lit True, N.Attribute "eid", TInt32)
-                         , (Lit True, N.Attribute "firstname",  TString)
-                         , (Lit True, N.Attribute "lastname",  TString)
-                         , (Lit True, N.Attribute "email_id", TString)
-                         , (Lit True, N.Attribute "folder", TString)
-                         , (Lit True, N.Attribute "status", TString)
-                         , (signature, N.Attribute "verification_key", TString)
-                         , (encryption,   N.Attribute "puclic_key", TString)
+employeelist_vrelation = [ (Lit True, eid_, TInt32)
+                         , (Lit True, firstname_,  TString)
+                         , (Lit True, lastname_,  TString)
+                         , (Lit True, email_id_, TString)
+                         , (Lit True, folder_, TString)
+                         , (Lit True, status_, TString)
+                         , (signature, verification_key_, TString)
+                         , (encryption,   puclic_key_, TString)
                          ]
                
-messages_vrelation = [ (Lit True, N.Attribute "mid", TInt32) 
-                     , (Lit True, N.Attribute "sender",  TString)
-                     , (Lit True, N.Attribute "date",  TString)
-                     , (Lit True, N.Attribute "message_id", TInt32)
-                     , (Lit True, N.Attribute "subject", TString)
-                     , (Lit True, N.Attribute "body", TString)
-                     , (Lit True, N.Attribute "folder", TString)
-                     , (Lit True, N.Attribute "is_system_notification", TBool)
-                     , (signature, N.Attribute "is_signed", TBool)
-                     , (encryption, N.Attribute"is_encrypted", TBool)
-                     , (autoresponder, N.Attribute"is_autoresponse", TBool)
-                     , (forwardmessages, N.Attribute"is_forward_msg", TBool)
+messages_vrelation = [ (Lit True, mid_, TInt32) 
+                     , (Lit True, sender_,  TString)
+                     , (Lit True, date_,  TString)
+                     , (Lit True, message_id_, TInt32)
+                     , (Lit True, subject_, TString)
+                     , (Lit True, body_, TString)
+                     , (Lit True, folder_, TString)
+                     , (Lit True, is_system_notification_, TBool)
+                     , (signature, is_signed_, TBool)
+                     , (encryption, is_encrypted_, TBool)
+                     , (autoresponder, is_autoresponse_, TBool)
+                     , (forwardmessages, is_forward_msg_, TBool)
                      ] 
 
 recipientinfo_vrelation = constructAllTrueRelSchema  [ ("rid", TInt32)
@@ -159,3 +193,159 @@ alias_vrelation = constructAllTrueRelSchema  [ ("eid", TInt32)
                                              , ("email",  TString)
                                              , ("nickname",  TString)
                                              ]
+
+--
+-- Pure Relational Schemas For Employee Evolution 
+--
+
+--  ** Schema Variant for Basic Email
+basicEmailSchema :: RSchema
+basicEmailSchema  = constructRSchema   [ ( employeelist,  basic_employeelist)
+                                       , ( messages,    basic_messages)
+                                       , ( recipientinfo,  recipientinfo_table)
+                                       , ( referenceinfo, referenceinfo_table)
+                                       ]
+-- | employeelist(eid, firstname, lastname, email_id , folder , status)
+basic_employeelist :: [(N.Attribute, SqlType)]
+basic_employeelist = [ ( eid_, TInt32)
+                     , ( firstname_,  TString)
+                     , ( lastname_,  TString)
+                     , ( email_id_, TString)
+                     , ( folder_, TString)
+                     , ( status_, TString)
+                     , ( verification_key_, TString)
+                     , ( puclic_key_, TString)
+                     ]
+-- | messages(mid, sender, date, message id , subject, body, folder,, is_system_notification)
+basic_messages :: [(N.Attribute, SqlType)]
+basic_messages = [ (mid_, TInt32) 
+                 , (sender_,  TString)
+                 , (date_,  TString)
+                 , (message_id_, TInt32)
+                 , (subject_, TString)
+                 , (body_, TString)
+                 , (folder_, TString)
+                 , (is_system_notification_, TBool)
+                 ] 
+
+-- | recipientinfo(rid, mid, rtype, rvalue)
+recipientinfo_table :: [(N.Attribute, SqlType)]
+recipientinfo_table =  [ (rid_, TInt32)
+                       , (mid_,  TInt32)
+                       , (rtype_,  TString)
+                       , (rvalue_, TString)
+                       ]
+-- | referenceinfo(rid, mid, reference)
+referenceinfo_table :: [(N.Attribute, SqlType)]
+referenceinfo_table =  [ (rfid_, TInt32)
+                       , (mid_,  TInt32)
+                       , (reference_,  TString)
+                       ]
+
+--  ** Schema Variant for Enhanced Email (basic + forwardmessages + filtermessages)
+enhancedEmailSchema :: RSchema
+enhancedEmailSchema  = constructRSchema   [ ( employeelist,  enhanced_employeelist)
+                                           , ( messages,    enhanced_messages)
+                                           , ( recipientinfo, recipientinfo_table)
+                                           , ( referenceinfo, referenceinfo_table)
+                                           , ( forward_msg, forward_msg_table)
+                                           , ( filter_msg, filter_msg_table)
+                                           ]
+
+enhanced_employeelist :: [(N.Attribute, SqlType)]
+enhanced_employeelist = basic_employeelist
+
+enhanced_messages :: [(N.Attribute, SqlType)]
+enhanced_messages = basic_messages ++ [(is_forward_msg_, TBool)]
+
+forward_msg_table :: [(N.Attribute, SqlType)]
+forward_msg_table = [ (eid_, TInt32)
+                    , (forwardaddr_,  TString)
+                    ]
+
+filter_msg_table :: [(N.Attribute, SqlType)]
+filter_msg_table =  [ (eid_, TInt32) 
+                    , (suffix_,  TString)
+                    ] 
+
+--  ** Schema Variant for Privacy-focus Email (basic + signature + encryption + remailmessage)
+privacyEmailSchema :: RSchema
+privacyEmailSchema  = constructRSchema   [ ( employeelist,  privacy_employeelist)
+                                         , ( messages,    privacy_messages)
+                                         , ( recipientinfo, recipientinfo_table)
+                                         , ( referenceinfo, referenceinfo_table)
+                                         , ( remail_msg, remail_msg_table)
+                                         ]
+privacy_employeelist :: [(N.Attribute, SqlType)]
+privacy_employeelist = basic_employeelist ++                          
+                        [ (verification_key_, TString)
+                        , (puclic_key_, TString)
+                        ]
+
+privacy_messages :: [(N.Attribute, SqlType)]
+privacy_messages = basic_messages ++                    
+                      [ (is_signed_, TBool)
+                      , (is_encrypted_, TBool)
+                      ]
+
+remail_msg_table :: [(N.Attribute, SqlType)]
+remail_msg_table = [ (eid_, TInt32) 
+                   , (pseudonym_,  TString)
+                   ]
+
+--  ** Schema Variant for Group Email 
+--     (basic + addressbook + signature + encryption + autoresponder + mailhost)
+groupEmailSchema :: RSchema
+groupEmailSchema  = constructRSchema   [ ( employeelist,  group_employeelist)
+                                         , ( messages,    group_messages)
+                                         , ( recipientinfo, recipientinfo_table)
+                                         , ( referenceinfo, referenceinfo_table)
+                                         , ( auto_msg, auto_msg_table)
+                                         , ( alias, alias_table)
+                                         , ( mail_host, mailhost_table)
+                                         ]
+group_employeelist :: [(N.Attribute, SqlType)]
+group_employeelist = privacy_employeelist
+
+group_messages  :: [(N.Attribute, SqlType)]
+group_messages = privacy_messages ++ [(is_autoresponse_, TBool)]
+
+sToAttribtue :: [(String, SqlType)] -> [(N.Attribute, SqlType)]
+sToAttribtue = map (\(x,y) -> (N.Attribute x , y))
+
+auto_msg_table  :: [(N.Attribute, SqlType)]
+auto_msg_table =  sToAttribtue [ ("eid", TInt32)
+                              , ("username",  TString)
+                              , ("mailhost",  TString)
+                              ]
+
+alias_table  :: [(N.Attribute, SqlType)]
+alias_table = sToAttribtue [ ("eid", TInt32)
+                           , ("email",  TString)
+                           , ("nickname",  TString)
+                           ]
+
+mailhost_table  :: [(N.Attribute, SqlType)]
+mailhost_table = sToAttribtue  [ ("eid", TInt32)
+                              , ("username",  TString)
+                              , ("mailhost",  TString)
+                              ]
+--  ** Schema Variant for Premium Email 
+premiumEmailSchema :: RSchema
+premiumEmailSchema  = constructRSchema   [ ( employeelist,  premium_employeelist)
+                                         , ( messages,    premium_messages)
+                                         , ( recipientinfo, recipientinfo_table)
+                                         , ( referenceinfo, referenceinfo_table)
+                                         , ( forward_msg, forward_msg_table)
+                                         , ( filter_msg, filter_msg_table)
+                                         , ( remail_msg, remail_msg_table)
+                                         , ( auto_msg, auto_msg_table)
+                                         , ( alias, alias_table)
+                                         , ( mail_host, mailhost_table)
+                                         ]
+                      where premium_employeelist = privacy_employeelist
+                            premium_messages = group_messages ++ [(is_forward_msg_, TBool)]
+
+
+
+
