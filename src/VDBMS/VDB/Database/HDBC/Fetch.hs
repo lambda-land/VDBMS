7841db4e07bc2158b6f8bc5aa817fef59f51c4a0
pataei <paris.ataei@gmail.com>
2020-01-27 12:36:58 -0800

    fetch

--- src/VDBMS/VDB/Database/HDBC/Fetch.hs
+++ src/VDBMS/VDB/Database/HDBC/Fetch.hs
@@ -5,18 +5,29 @@ import VDBMS.VDB.Schema.Variational.Schema
 import VDBMS.VDB.Name
 import VDBMS.VDB.Database.Database
 import VDBMS.VDB.Table.Table
-
+import VDBMS.DBMS.Table.Table (SqlTable)
 import Database.HDBC 
+
+
+-- data Table = Table TableSchema SqlTable
+-- type TableSchema = Opt RowType
+-- type RowType = Map Attribute (Opt SqlType)
+-- type Opt a = (FeatureExpr, a)
+-- type SqlTable = [SqlRow]
+-- type SqlRow = Map String SqlValue
 
 
 -- | fetchs result of a query in a table for any 
 --   instance of IConnection.
-fetch :: IConnection conn => conn -> Query -> IO Table
-fetch c q = undefined
-  -- do s <- prepare c q
-  --    t <- fetchAllRowsMap s 
+fetch :: IConnection conn => conn -> Query -> IO SqlTable
+fetch c q = 
+  do s <- prepare c q
+     fetchAllRowsMap s 
      
 
 -- | stirct version of fetch.
-fetch' :: IConnection conn => conn -> Query -> IO Table
-fetch' = undefined
+fetch' :: IConnection conn => conn -> Query -> IO SqlTable
+fetch' c q = 
+  do s <- prepare c q
+     fetchAllRowsMap' s
+
