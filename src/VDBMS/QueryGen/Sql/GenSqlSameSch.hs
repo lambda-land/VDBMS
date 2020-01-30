39ab20a7e76ac1c691f1ca981c79cda64fb4683a
pataei <paris.ataei@gmail.com>
2020-01-30 14:16:47 -0800

    gen sql same sch

---
+++ src/VDBMS/QueryGen/Sql/GenSqlSameSch.hs
@@ -0,0 +1,26 @@
+-- | generates one sql query from sql queries/ra queries with different schemas
+module VDBMS.QueryGen.Sql.GenSqlSameSch (
+
+       optRAQs2Sql
+
+) where 
+
+import VDBMS.QueryLang.RelAlg.Relational.Algebra (RAlgebra)
+import VDBMS.QueryLang.SQL.Pure.Sql (SqlSelect(..), SqlBinOp(..))
+-- import VDBMS.TypeSystem.Relational.TypeSystem (RTypeEnv, rtypeEnvAtts)
+import VDBMS.VDB.Name (Attribute, PCatt)
+import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
+import VDBMS.QueryLang.SQL.Pure.Ops
+import VDBMS.Variational.Opt (Opt, mapSnd)
+
+-- | takes a list of opt RA queries and generate one sql query.
+optRAQs2Sql :: [Attribute] -> PCatt -> [Opt RAlgebra] -> SqlSelect
+optRAQs2Sql as pc qs = foldl (SqlBin SqlUnionAll) 
+                             (head sqls_updatedPC_sameSch) 
+                             (tail sqls_updatedPC_sameSch)
+  where
+    sqls = mapSnd transAlgebra2Sql qs
+    sqls_updatedPC_sameSch = map 
+      (\(f,q) -> updatePC pc (adjustQSch as (sqlQAtts q) q) f) 
+      sqls
+
