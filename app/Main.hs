e70fe057abca4f5562ac45bdb57f059b6f03e1df
pataei <paris.ataei@gmail.com>
2020-02-18 15:31:08 -0800

    main ready to do experiments

--- app/Main.hs
+++ app/Main.hs
@@ -1,101 +1,37 @@
 module Main where
 
-import VDBMS.DBsetup.Postgres.EnronEmailDB
 import VDBMS.VDB.Database.HDBC.PostgreSQL
+import VDBMS.VDB.Table.Table
 
--- import System.Directory
--- import System.Clock
--- import System.CPUTime
--- import Text.Printf
+-- Enron email VDB
+import VDBMS.DBsetup.Postgres.EnronEmailDB
+import VDBMS.UseCases.EnronUseCase.EnronQuery
 
-{-- uncomment after modularization!
+-- Employee VDB
+import VDBMS.DBsetup.Postgres.EmployeeDB
+import VDBMS.UseCases.EmployeeUseCase.EmployeeQuery
 
-time :: IO t -> IO t
-time a = do
-    start <- getCPUTime
-    v <- a
-    end   <- getCPUTime
-    let diff = (fromIntegral (end - start)) / (10^12)
-    printf "Computation time: %0.5f sec\n" (diff :: Double)
-    return v
 
--- main :: IO VTable
--- main :: IO [[SqlValue]]
-main = do 
-  -- path <- getCurrentDirectory
-  -- testdb <- connectSqlite3 "/databases/testDB/test1.db"
-  -- /Volumes/GoogleDrive/My Drive/OSU/Research/VDBMSgit/codes/VDBMS/databases/employeeDB/emp_vdb.db
-  -- employeeConn <- connectSqlite3 "./databases/employeeDB/emp_vdb.db"
-  -- conn <- employeeConn
-  vdb <- employeeVDB
-  -- let p = PresCondAtt "presCond"
-  --     employeeVSchema = variationizeSchema [empSchema1, empSchema2, empSchema3, empSchema4, empSchema5]
-  --     employeeVDB = VDB employeeVSchema employeeConn
-      -- vqManual = AChc (Ref $ Feature "v1") empQ1_v1 
-      --            (AChc (Or (Ref $ Feature "v2") (Ref $ Feature "v3")) empQ1_v2 
-      --             empQ1_v4and5)
-  -- configVDB "./databases/employeeDB/confed/emp_confed" p vdb c1 1
-  -- configVDB "./databases/employeeDB/confed/emp_confed" p vdb c2 2 
-  -- configVDB "./databases/employeeDB/confed/emp_confed" p vdb c3 3
-  -- configVDB "./databases/employeeDB/confed/emp_confed" p vdb c4 4
-  -- configVDB "./databases/employeeDB/confed/emp_confed" p vdb c5 5
-  configVDBall "./databases/employeeDB/confed/emp_confed" p vdb [c1, c2, c3, c4, c5]
-  --     q = empQ2_v1
-      -- qualifyQuery employeeVSchema $ variationizeQuery [empQ2_v1]
-      -- vq = variationizeQuery [empQ1_v1]
-      -- , empQ1_v2, empQ1_v3, empQ1_v4, empQ1_v5]
-      -- qualifiedVq = qualifyQuery employeeVSchema vq
-      -- vqManualEmpQ1_v1 = AChc (Ref $ Feature "v1") empQ1_v1 Empty
-      -- dbs = [db1,db2,db3,db4,db5]
-  -- runTransFilterUnion vqManualEmpQ1_V1 p employeeVDB -- WORKS!!! HAVE NO IDEA IF IT'S CORRECT THO!!
-  -- runTransFilterUnion vqManual p employeeVDB --WORKS!! TIME IT FOR SUBMISSION!!!
-  -- runTransFilterUnion vq p employeeVDB
-  -- Count time here ----------------------------
-  -- putStrLn "Starting..."
-  -- time $ runTransFilterUnion vqManual p employeeVDB -- 4.08534 sec, 4.25021 sec
-  -- putStrLn "Done."
-  --     configs = [c1,c2,c3,c4,c5]
-  --     q1 = "select empno, name, hiredate from v_engineerpersonnel union select empno, name, hiredate from v_otherpersonnel;"
-  --     q2 = "select empno, name, hiredate from v_empacct;"
-  --     q3 = "select empno, name, hiredate from v_empacct;"
-  --     q4 = "select v_empacct.empno, name, hiredate from v_empacct inner join v_empbio;"
-  --     q5 = "select v_empacct.empno, firstname, lastname, hiredate from v_empacct inner join v_empbio;"
-  --     -- qs = [q1,q2,q3,q4,q5]
-  --     -- vqs = zip qs configs
-  -- putStrLn "Starting..."    
-  -- conn1 <- connectSqlite3 "./databases/employeeDB/relDBs/emp1.db"
-  -- let db1 = VariantDB empSchema1 (conn1,c1)
-  -- time $ runq c1 q1 db1
-  -- disconnect conn1
-  -- conn2 <- connectSqlite3 "./databases/employeeDB/relDBs/emp2.db"
-  -- let db2 = VariantDB empSchema2 (conn2,c2)
-  -- time $ runq c2 q2 db2
-  -- disconnect conn2
-  -- conn3 <- connectSqlite3 "./databases/employeeDB/relDBs/emp3.db"
-  -- let db3 = VariantDB empSchema3 (conn3,c3)
-  -- time $ runq c3 q3 db3
-  -- disconnect conn3
-  -- conn4 <- connectSqlite3 "./databases/employeeDB/relDBs/emp4.db"
-  -- let db4 = VariantDB empSchema4 (conn4,c4)
-  -- time $ runq c4 q4 db4
-  -- disconnect conn4
-  -- conn5 <- connectSqlite3 "./databases/employeeDB/relDBs/emp5.db"
-  -- let db5 = VariantDB empSchema5 (conn5,c5)
-  -- time $ runq c5 q5 db5
-  -- disconnect conn5
-  -- putStrLn "Done."
+-- approaches
+import VDBMS.Approaches.ConfigQ.RunVariantQueryOnVDB (runQ1)
+import VDBMS.Approaches.ConfigQ.RunVariantQueryOnVDBConcurrent (runQ4)
+import VDBMS.Approaches.Linearize.RunOneQueryAtATime (runQ2)
+import VDBMS.Approaches.Linearize.RunOneBigQuery (runQ3)
+import VDBMS.Approaches.Linearize.RunQsConcurrent (runQ5)
+-- runQ_i :: Database conn => conn -> Algebra -> IO Table
 
-  -- putStrLn "Starting..."    
-  -- time $ runSqlQsOnCorrespDBs vqs dbs --23.47896 sec
-  -- putStrLn "Done."
+-- | Enron experiment.
+main :: IO Table
+main = 
+  do enronvdb <- enronVDB
+     -- runQ1 enronvdb q
+     return undefined
 
-  -- putStrLn "Starting..."    
-  -- time $ bruteForSigmod vqManual configs dbs -- 105.11169 sec, 108.28369 sec, 100.95015 sec, 23.47896 sec
-  -- putStrLn "Done."
-  -- runBrute' q configs p "./databases/employeeDB/relDBs/emp_db" employeeVDB
-  
---}
+-- -- | Employee experiment.
+-- main :: IO Table
+-- main = 
+--   do empvdb <- employeeVDB
+--      -- runQ1 empvdb q 
+--      return undefined
 
-main :: IO PostgresHDBC
-main = enronVDB
 -- main = return ()
