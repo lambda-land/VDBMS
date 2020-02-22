d863e80e882d17977220f8037a7874465a47c4f7
pataei <paris.ataei@gmail.com>
2020-02-21 16:18:24 -0800

    remove things that were creating problems

--- databases/enronEmailDB/step1_build_enron_variants.sql
+++ databases/enronEmailDB/step1_build_enron_variants.sql
@@ -105,16 +105,16 @@ CREATE OR REPLACE view auto_msg_view as
 SELECT eid, 
 CASE    
 WHEN  eid% 3 = 0 THEN  "out of office"
-WHEN  eid% 3 = 1 THEN "Thanks for getting in touch. We’re on it."
+WHEN  eid% 3 = 1 THEN "Thanks for getting in touch. We are on it."
 WHEN  eid% 3 = 2 THEN "out of office"
 END AS subject,
 CASE
 WHEN  eid% 3 = 0 THEN  "Hi,
-I'm enjoying a holiday at sea and will be off the grid until the 15th of January! I'll get back to you that week. You could also reach out to my colleagues via support@email.com.
+I am enjoying a holiday at sea and will be off the grid until the 15th of January! I will get back to you that week. You could also reach out to my colleagues via support@email.com.
 Thanks for your patience and talk to you then!
 Best regards,
 "
-WHEN  eid% 3 = 1 THEN "Thanks for getting in touch. We’ll get back to you within 6 business hours (Monday-Friday 9am-6pm EST)."
+WHEN  eid% 3 = 1 THEN "Thanks for getting in touch. We will get back to you within 6 business hours (Monday-Friday 9am-6pm EST)."
 WHEN  eid% 3 = 2 THEN "Thank you for your message. I am currently out of the office, with no email access. If you need immediate assistance before then, you may reach me at my mobile"
 END AS body 
 FROM employeelist
