5d51edc5636ea92079e0d959b7e8a423b21aad03
pataei <paris.ataei@gmail.com>
2020-02-26 14:12:44 -0800

    comment

--- src/VDBMS/DBsetup/Postgres/EmployeeDB.hs
+++ src/VDBMS/DBsetup/Postgres/EmployeeDB.hs
@@ -13,6 +13,7 @@ import Database.HDBC.PostgreSQL
 -- https://www.postgresql.org/docs/8.1/libpq.html#LIBPQ-CONNECT
 empConn :: String 
 empConn = "host=localhost dbname=employees user=postgres password=paris1993"
+-- empConn = "host=localhost dbname=employees user=postgres password=paris1993"
 
 -- | employee VDB
 employeeVDB :: IO PostgresHDBC
