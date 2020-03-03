8495b5789e1c2802f5bb7053e100d9757e2173ab
pataei <paris.ataei@gmail.com>
2020-03-03 11:10:09 -0800

    trying a couple of things for test and benchmark

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,38 @@
 module Main where
 
-import VDBMS.VDB.Database.HDBC.PostgreSQL
-import VDBMS.VDB.Table.Table
+-- import VDBMS.VDB.Database.HDBC.PostgreSQL
+-- import VDBMS.VDB.Table.Table
 
--- Enron email VDB
-import VDBMS.DBsetup.Postgres.EnronEmailDB
-import VDBMS.UseCases.EnronUseCase.EnronQuery
+-- -- Enron email VDB
+-- import VDBMS.DBsetup.Postgres.EnronEmailDB
+-- import VDBMS.UseCases.EnronUseCase.EnronQuery
 
--- Employee VDB
-import VDBMS.DBsetup.Postgres.EmployeeDB
-import VDBMS.UseCases.EmployeeUseCase.EmployeeQuery
+-- -- Employee VDB
+-- import VDBMS.DBsetup.Postgres.EmployeeDB
+-- import VDBMS.UseCases.EmployeeUseCase.EmployeeQuery
 
 
--- approaches
-import VDBMS.Approaches.ConfigQ.RunVariantQueryOnVDB (runQ1)
-import VDBMS.Approaches.ConfigQ.RunVariantQueryOnVDBConcurrent (runQ4)
-import VDBMS.Approaches.Linearize.RunOneQueryAtATime (runQ2)
-import VDBMS.Approaches.Linearize.RunOneBigQuery (runQ3)
-import VDBMS.Approaches.Linearize.RunQsConcurrent (runQ5)
--- runQ_i :: Database conn => conn -> Algebra -> IO Table
+-- -- approaches
+-- import VDBMS.Approaches.ConfigQ.RunVariantQueryOnVDB (runQ1)
+-- import VDBMS.Approaches.ConfigQ.RunVariantQueryOnVDBConcurrent (runQ4)
+-- import VDBMS.Approaches.Linearize.RunOneQueryAtATime (runQ2)
+-- import VDBMS.Approaches.Linearize.RunOneBigQuery (runQ3)
+-- import VDBMS.Approaches.Linearize.RunQsConcurrent (runQ5)
+-- -- runQ_i :: Database conn => conn -> Algebra -> IO Table
+
+import VDBMS.UseCases.EmployeeUseCase.Test
+
+-- | Employee test vdb.
+main :: IO Bool
+main = testEmpVDB
+
 
 -- | Enron experiment.
-main :: IO Table
-main = 
-  do enronvdb <- enronVDB
-     -- runQ1 enronvdb q
-     return undefined
+-- main :: IO Table
+-- main = 
+--   do enronvdb <- enronVDB
+--      -- runQ1 enronvdb q
+--      return undefined
 
 -- -- | Employee experiment.
 -- main :: IO Table
