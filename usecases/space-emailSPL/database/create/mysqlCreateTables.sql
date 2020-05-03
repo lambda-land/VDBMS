bcacba8e3f794b20134826decfed904c01a8bd60
pataei <paris.ataei@gmail.com>
2020-05-03 00:52:00 -0700

    create queries

---
+++ usecases/space-emailSPL/database/create/mysqlCreateTables.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d10bb2ae15c3f21c944baa3c5a26e8d4d176883a0ec1262b3846cda1234ada22
+size 1927
---
+++ usecases/time-employee/database/create/mysqlCreateTables.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80640f0f68dfab116bfbe41d8f45b7a5843c0e392e8962ee7dbf6896665af756
+size 1427
