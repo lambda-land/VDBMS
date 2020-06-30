6fe3aeb13d418a1f52b2c19c89d83a0ae6685e2e
pataei <paris.ataei@gmail.com>
2020-06-29 21:03:38 -0700

    fix empvq5alt

--- usecases/time-employee/queries/EmployeeQueriesIfdefSql.sql
+++ usecases/time-employee/queries/EmployeeQueriesIfdefSql.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e661750f357437724fb54459b4051cd935c3b6cccfd3fd4f17519df2f39fa594
-size 10028
+oid sha256:99c6efea9935b580a26b6441653b6d93c7688c8837e4a97122b6e6c8bba3d56b
+size 10035
