a4ae978af20eb92fb82b40da7cebae0109c41def
pataei <paris.ataei@gmail.com>
2020-09-03 19:26:29 -0700

    add files

---
+++ usecases/test/create.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9a076241a121fd623b636664f9e619e874b70685ad7c20f6306b5b16284e425f
+size 481
---
+++ usecases/test/insert.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4008c9bbad26cad0b44bb31eaed7a0bd17d1ad2edd4fe0d5d8a7de7519af8183
+size 571
