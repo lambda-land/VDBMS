8168badb04b5b545fb85896acf004c47ca41e97d
pataei <paris.ataei@gmail.com>
2020-09-15 21:11:43 -0700

    added pc to test vdb

--- usecases/test/insert.sql
+++ usecases/test/insert.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52f54ff50298e187f975524ea23f62987afb3d8bb369981e98df8fb31792849b
-size 640
+oid sha256:92e00cdb4a189afd8adf2bcd3696307e70a080dba9d3609bcec40604d35bf110
+size 869
