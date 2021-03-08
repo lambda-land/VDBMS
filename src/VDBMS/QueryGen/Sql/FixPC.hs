7d567af78c76453170f9478675c9890d0561d7d0
pataei <paris.ataei@gmail.com>
2021-03-07 23:06:43 -0800

    will get back to it after folding laundry

--- src/VDBMS/QueryGen/Sql/FixPC.hs
+++ src/VDBMS/QueryGen/Sql/FixPC.hs
@@ -3,11 +3,11 @@
 module VDBMS.QueryGen.Sql.FixPC where 
 
 -- -- import VDBMS.QueryLang.RelAlg.Relational.Algebra 
--- import VDBMS.QueryLang.SQL.Pure.Sql
+import VDBMS.QueryLang.SQL.Pure.Sql
 -- import VDBMS.QueryLang.SQL.Condition (SqlCond(..),RCondition(..))
 -- import VDBMS.QueryLang.RelAlg.Relational.Algebra
 -- -- import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
--- import VDBMS.VDB.Name 
+import VDBMS.VDB.Name 
 -- -- import VDBMS.QueryLang.SQL.Condition 
 -- -- import VDBMS.QueryLang.RelAlg.Basics.SetOp
 -- -- import VDBMS.VDB.Schema.Variational.Schema
@@ -22,3 +22,22 @@ module VDBMS.QueryGen.Sql.FixPC where
 -- import qualified Data.Map.Strict as SM
 
 -- import Debug.Trace
+
+-- |
+fixPC :: Sql -> Sql 
+fixPC (Sql sfw) = Sql $ fixPCsfw sfw
+fixPC (SqlBin o l r) = SqlBin o (fixPC l) (fixPC r)
+fixPC sql = sql  
+
+
+-- |
+fixPCsfw :: SelectFromWhere -> SelectFromWhere
+fixPCsfw (SelectFromWhere as ts cs) = SelectFromWhere as' ts' cs
+  where
+    as' = undefined
+    ts' = map fixPCrenameSqlRelation ts
+
+-- |
+fixPCrenameSqlRelation :: Rename SqlRelation -> Rename SqlRelation
+fixPCrenameSqlRelation = undefined
+
