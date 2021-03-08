78733ad70fb525e7707d2c0206b62c8c97670966
pataei <paris.ataei@gmail.com>
2021-03-07 22:51:44 -0800

    add fix pc

---
+++ src/VDBMS/QueryGen/Sql/FixPC.hs
@@ -0,0 +1,24 @@
+-- | generates sql queries from sql queries/ra queries
+--   and renames subqueries/relations without an alias.
+module VDBMS.QueryGen.Sql.FixPC where 
+
+-- -- import VDBMS.QueryLang.RelAlg.Relational.Algebra 
+-- import VDBMS.QueryLang.SQL.Pure.Sql
+-- import VDBMS.QueryLang.SQL.Condition (SqlCond(..),RCondition(..))
+-- import VDBMS.QueryLang.RelAlg.Relational.Algebra
+-- -- import VDBMS.QueryTrans.AlgebraToSql (transAlgebra2Sql)
+-- import VDBMS.VDB.Name 
+-- -- import VDBMS.QueryLang.SQL.Condition 
+-- -- import VDBMS.QueryLang.RelAlg.Basics.SetOp
+-- -- import VDBMS.VDB.Schema.Variational.Schema
+
+-- -- import Data.List ((\\))
+
+-- import Control.Monad.State 
+
+-- import Data.Maybe (isNothing, fromJust)
+
+-- import Data.Map.Strict (Map)
+-- import qualified Data.Map.Strict as SM
+
+-- import Debug.Trace
